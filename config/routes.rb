3cd43167be55608b725d5a3df6659310909a2218
Christian Kruse <cjk@wwwtech.de>
2012-06-20 20:30:44 +0200

    implemented baisc routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Cforum::Application.routes.draw do
+
+  root to: 'threads#index'
+  match '/' => 'threads#create', :via => :put
+  match '/:year/:mon/:day/:tid' => 'threads#view', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, :via => :get
+  match '/:year/:mon/:day/:tid' => 'threads#destroy', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, :via => :delete
+
+  match '/:year/:mon/:day/:tid/:mid' => 'messages#view', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, :via => :get
+  match '/:year/:mon/:day/:tid/:mid' => 'messages#update', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, :via => :put
+  match '/:year/:mon/:day/:tid/:mid' => 'messages#destroy', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, :via => :delete
+  match '/:year/:mon/:day/:tid' => 'messages#create', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, :via => :put
+
+  match '/users' => 'users#index', :via => :get
+  match '/users' => 'users#create', :via => :put
+  match '/users/:username' => 'users#view', :via => :get
+  match '/users/:username' => 'users#update', :via => :put
+  match '/users/:username' => 'users#destroy', :via => :delete
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
