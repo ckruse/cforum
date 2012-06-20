db020363a0d0bca5cd40724d4de3ae146053b83c
Jeena Paradies <spam@jeenaparadies.net>
2012-06-21 00:08:32 +0200

    made the url helpers work

--- app/helpers/url_helpers.rb
+++ app/helpers/url_helpers.rb
@@ -1,30 +1,32 @@
-def threads_path
-  root_path
-end
-def thread_path(thread)
-  thread.id
-end
-def edit_thread_path(thread)
-  thread.id + '/edit'
-end
-def new_thread_path
-  '/new'
-end
+module UrlHelpers
+
+    def threads_path
+      root_path
+    end
+    def thread_path(thread)
+      thread.id
+    end
+    def edit_thread_path(thread)
+      thread.id + '/edit'
+    end
+    def new_thread_path
+      '/new'
+    end
 
 
-def messages_path(thread)
-  thread.id
-end
-def message_path(thread, message)
-  thread.id + "/" + message.mid
-end
-def edit_message_path(thread, message)
-  thread.id + "/" + message.mid + "/edit"
-end
-def new_message_path(thread)
-  thread.id + "/new"
-end
+    def messages_path(thread)
+      thread.id
+    end
+    def message_path(thread, message)
+      thread.id + "/" + message.mid
+    end
+    def edit_message_path(thread, message)
+      thread.id + "/" + message.mid + "/edit"
+    end
+    def new_message_path(thread)
+      thread.id + "/new"
+    end
 
-
+end
 
 # eof
