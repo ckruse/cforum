c73ee6318d6b5ec523cc1291c849f384facd601c
Christian Kruse <cjk@wwwtech.de>
2012-06-23 01:41:52 +0200

    fixed settings selection

--- lib/config_manager.rb
+++ lib/config_manager.rb
@@ -10,12 +10,13 @@ class ConfigManager
     setting = nil
 
     unless user.nil?
-      setting = CForum::Setting.find(:user => user, :id => name)
+      usr = CForum::User.find_by_id(user)
+      setting = usr.settings[name] if usr and usr.settings[name]
     else
       if @@value_cache.has_key?(name) and @@value_cache[name][:expiry] > Time.now
         setting = @@value_cache[name][:value]
       else
-        setting = CForum::Setting.find(:id => name)
+        setting = CForum::Setting.find_by_id(name)
         @@value_cache[name] = {
           expiry: Time.now + 60,
           value: setting
