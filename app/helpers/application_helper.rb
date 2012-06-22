af7370843bc24a07a9818be420f09add6ce267cb
Christian Kruse <cjk@wwwtech.de>
2012-06-22 22:47:56 +0200

    fixed whitespaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,3 @@
 module ApplicationHelper
-    include UrlHelpers
+  include UrlHelpers
 end
--- app/helpers/url_helpers.rb
+++ app/helpers/url_helpers.rb
@@ -1,31 +1,30 @@
 module UrlHelpers
+  def threads_path
+    root_path
+  end
+  def thread_path(thread)
+    thread.id
+  end
+  def edit_thread_path(thread)
+    thread.id + '/edit'
+  end
+  def new_thread_path
+    '/new'
+  end
 
-    def threads_path
-      root_path
-    end
-    def thread_path(thread)
-      thread.id
-    end
-    def edit_thread_path(thread)
-      thread.id + '/edit'
-    end
-    def new_thread_path
-      '/new'
-    end
 
-
-    def messages_path(thread)
-      thread.id
-    end
-    def message_path(thread, message)
-      thread.id + "/" + message.id
-    end
-    def edit_message_path(thread, message)
-      thread.id + "/" + message.id + "/edit"
-    end
-    def new_message_path(thread)
-      thread.id + "/new"
-    end
+  def messages_path(thread)
+    thread.id
+  end
+  def message_path(thread, message)
+    thread.id + "/" + message.id
+  end
+  def edit_message_path(thread, message)
+    thread.id + "/" + message.id + "/edit"
+  end
+  def new_message_path(thread)
+    thread.id + "/new"
+  end
 
 end
 
