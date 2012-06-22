e59eb57ab002b24daf6bea9cec5fcc5fb0c8d35c
Christian Kruse <cjk@wwwtech.de>
2012-06-22 17:50:16 +0200

    added method find_message
    added a method find_message(mid) to find a message in a thread

--- app/models/c_forum/thread.rb
+++ app/models/c_forum/thread.rb
@@ -8,6 +8,17 @@ module CForum
     key :archived, Boolean
 
     one :message, :class_name => 'CForum::Message'
+
+    def find_message(mid, msg = nil)
+      msg = message if msg.nil?
+      return msg if msg.id == mid
+
+      unless messages.blank?
+        messages.each do |m|
+          return found if found = find_message(mid, m)
+        end
+      end
+    end
   end
 end
 
