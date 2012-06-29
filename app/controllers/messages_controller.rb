e7508a593f8717b20c4fa307f1fea29c23d697e9
Christian Kruse <cjk@wwwtech.de>
2012-06-29 12:12:37 +0200

    code cleaning

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,9 @@
 class MessagesController < ApplicationController
   def show
-    found = false
     @id = '/' + params[:year] + '/' + params[:mon] + '/' + params[:day] + '/' + params[:tid]
     @thread = CForum::Thread.find_by_id(@id)
 
-    if @thread
-      @message = @thread.find_message(params[:mid])
-      found = true if @message
-    end
-
-    if @thread.nil? or @message.nil?
-      raise CForum::NotFoundException.new
-    end
+    @message = @thread.find_message(params[:mid]) if @thread
+    raise CForum::NotFoundException.new if @thread.nil? or @message.nil?
   end
 end
