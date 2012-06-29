40f3805796eee0425d80f50ce7b1d22dcf47e5f2
Christian Kruse <cjk@wwwtech.de>
2012-06-29 14:37:13 +0200

    implemented user creation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = CForum::User.new
+  end
+
+  def create
+    @user = CForum::User.new(params[:c_forum_user])
+
+    if @user.save
+      redirect_to root_url, :notice => I18n.t('users.signed_up')
+    else
+      render :new
+    end
+  end
+end
+
+# eof
