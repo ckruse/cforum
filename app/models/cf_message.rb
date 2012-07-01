0d179aad4ae0996ee10f134d191261a40dcbba16
Christian Kruse <cjk@wwwtech.de>
2012-07-02 01:53:46 +0200

    removed comments

--- app/models/cf_message.rb
+++ app/models/cf_message.rb
@@ -9,15 +9,11 @@ class CfMessage
   field :category, type: String
   field :content, type: String
 
-  #timestamps!
-
   field :flags, type: Hash
 
   embeds_one :author, :class_name => 'CfAuthor'
-  #one :author, :class_name => 'CfAuthor'
 
   embeds_many :messages, :class_name => 'CfMessage'
-  #many :messages, :class_name => 'CfMessage'
 
   validates_presence_of :id, :subject, :category, :content
   validates_associated :author
