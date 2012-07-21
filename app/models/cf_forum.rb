d0476dccad5c6587571425be1f7353a830dd3111
Christian Kruse <cjk@wwwtech.de>
2012-07-21 18:16:53 +0200

    fix: added missing attribute „public”

--- app/models/cf_forum.rb
+++ app/models/cf_forum.rb
@@ -9,7 +9,7 @@ class CfForum < ActiveRecord::Base
   has_many :forum_mods, class_name: 'CfModerator', :foreign_key => :forum_id
   has_many :moderators, class_name: 'CfUser', :through => :forum_mods, :source => :user
 
-  attr_accessible :forum_id, :slug, :name, :short_name, :description, :updated_at, :created_at
+  attr_accessible :forum_id, :slug, :name, :short_name, :description, :updated_at, :created_at, :public
 
   validates :slug, presence: true, uniqueness: true, length: {:in => 3..20}, allow_blank: false, format: {with: /^[a-z0-9_-]+$/}
   validates :name, presence: true, length: {:minimum => 3}, allow_blank: false
