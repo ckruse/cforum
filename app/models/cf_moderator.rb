4407696043491abe66f23b0996a8aa79c646ebb9
Christian Kruse <cjk@wwwtech.de>
2012-07-21 14:16:38 +0200

    added attr declarations for attributes

--- app/models/cf_moderator.rb
+++ app/models/cf_moderator.rb
@@ -4,6 +4,8 @@ class CfModerator < ActiveRecord::Base
 
   belongs_to :user, class_name: 'CfUser', :foreign_key => :user_id
   belongs_to :forum, class_name: 'CfForum', :foreign_key => :forum_id
+
+  attr_accessible :user_id, :forum_id
 end
 
 
