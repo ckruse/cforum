9d5c9affd956486cd7e7b0fc9d264dd168800e03
Christian Kruse <cjk@wwwtech.de>
2012-08-04 18:35:48 +0200

    renamed sql file

--- db/migrate/0013_create_counter_framework.rb
+++ db/migrate/0013_create_counter_framework.rb
@@ -2,18 +2,13 @@
 
 class CreateCounterFramework  < ActiveRecord::Migration
   def up
-    sql = IO.read(File.dirname(__FILE__) + '/../count_threads.sql')
+    sql = IO.read(File.dirname(__FILE__) + '/../counter_table.sql')
     execute sql
   end
 
   def down
-    execute 'DROP FUNCTION cforum.counter_table_remove_count_trigger(v_table_name name, v_crit_column name, v_group_crit BIGINT)'
-    execute 'DROP FUNCTION cforum.counter_table_create_count_trigger(v_table_name name, v_crit_column name, v_group_crit BIGINT)'
     execute 'DROP FUNCTION cforum.counter_table_get_count(v_table_name NAME, v_group_crit BIGINT)'
-    execute 'DROP FUNCTION cforum.counter_table__truncate_trigger() CASCADE'
-    execute 'DROP FUNCTION cforum.counter_table__delete_trigger() CASCADE'
-    execute 'DROP FUNCTION cforum.counter_table__insert_trigger() CASCADE'
-    execute 'DROP TABLE cforum.counter_table CASCADE'
+    execute 'DROP TABLE cforum.counter_table'
   end
 end
 
