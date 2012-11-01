27ec6b93af9e52064e202d275eaec20a90a17164
Christian Kruse <cjk@defunced.de>
2012-11-01 22:03:21 +0100

    code cleanup and bugfixes

--- db/count_threads.sql
+++ db/count_threads.sql
@@ -24,7 +24,6 @@ CREATE TRIGGER threads__count_insert_trigger
 -- DELETE: add one dataset with difference = -1 for each row
 --
 CREATE FUNCTION cforum.count_threads_delete_trigger() RETURNS trigger LANGUAGE plpgsql AS $body$
-DECLARE field_val_v BIGINT;
 BEGIN
   INSERT INTO
     cforum.counter_table (table_name, difference, group_crit)
@@ -64,44 +63,21 @@ CREATE TRIGGER threads__count_truncate_trigger
   EXECUTE PROCEDURE cforum.count_threads_truncate_trigger();
 
 --
--- DELETE forum: remove counting datasets for this specific forum group
+-- INSERT forum: create a row for this forum group
 --
-CREATE FUNCTION cforum.count_threads_delete_forum_trigger() RETURNS trigger LANGUAGE plpgsql AS $body$
+CREATE FUNCTION cforum.count_threads_insert_forum_trigger() RETURNS trigger LANGUAGE plpgsql AS $body$
 BEGIN
-  DELETE FROM
-    cforum.counter_table
-  WHERE
-      table_name = 'threads'
-    AND
-      group_crit = OLD.forum_id;
+  INSERT INTO
+    cforum.counter_table (table_name, group_crit, difference)
+    VALUES ('threads', NEW.forum_id, 0);
 
   RETURN NULL;
 END;
 $body$;
 
-CREATE TRIGGER threads__count_delete_forum_trigger
-  AFTER DELETE
+CREATE TRIGGER threads__count_insert_forum_trigger
+  AFTER INSERT
   ON cforum.forums
-  FOR EACH ROW
-  EXECUTE PROCEDURE cforum.count_threads_delete_forum_trigger();
-
---
--- TRUNCATE forum: remove counting datasets for all forums
---
-CREATE FUNCTION cforum.count_threads_truncate_forum_trigger() RETURNS trigger LANGUAGE plpgsql AS $body$
-BEGIN
-  DELETE FROM
-    cforum.counter_table
-  WHERE
-    table_name = 'threads';
-
-  RETURN NULL;
-END;
-$body$;
-
-CREATE TRIGGER threads__count_truncate_forum_trigger
-  AFTER TRUNCATE
-  ON cforum.forums
-  EXECUTE PROCEDURE cforum.count_threads_truncate_forum_trigger();
+  EXECUTE PROCEDURE cforum.count_threads_insert_forum_trigger();
 
 -- eof
