eb48366126f251c57f0eeb13e6f864e6412c1a7b
Christian Kruse <cjk@defunced.de>
2012-11-04 12:32:47 +0100

    fixed settings model

--- app/models/cf_setting.rb
+++ app/models/cf_setting.rb
@@ -4,10 +4,16 @@ class CfSetting < ActiveRecord::Base
   self.primary_key = 'setting_id'
   self.table_name  = 'cforum.settings'
 
+  serialize :options, ActiveRecord::Coders::Hstore
+
   belongs_to :forum, class_name: 'CfForum', :foreign_key => :forum_id
   belongs_to :user, class_name: 'CfUser', :foreign_key => :user_id
 
-  attr_accessible :setting_id, :forum_id, :user_id, :name,  :value
+  attr_accessible :setting_id, :forum_id, :user_id, :options
+
+  after_initialize do
+    self.options ||= {} if attributes.has_key? 'options'
+  end
 end
 
 # eof
