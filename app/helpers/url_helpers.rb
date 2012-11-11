f93989be677b726a4b3e5a70ac49b44f47bb8fb5
Christian Kruse <cjk@defunced.de>
2012-11-11 03:37:48 +0100

    added query string handling to url and path helpers

--- app/helpers/url_helpers.rb
+++ app/helpers/url_helpers.rb
@@ -1,35 +1,47 @@
 module UrlHelpers
-  def cf_forum_path(forum)
-    return '/' + forum.slug
+  def query_string(args = {})
+    qs = []
+    args.each do |k, v|
+      qs << URI.escape(k.to_s) + "=" + URI.escape(v.to_s)
+    end
+
+    return '?' + qs.join("&") unless qs.blank?
+    ''
   end
 
-  def cf_thread_path(thread)
-    cf_forum_path(thread.forum) + thread.slug
-  end
-  def edit_cf_thread_path(thread)
-    cf_thread_path(thread) + '/edit'
+  def cf_forum_path(forum, args = {})
+    forum = forum.slug unless forum.is_a?(String)
+    root_path + forum + query_string(args)
   end
 
-  def cf_message_path(thread, message)
-    cf_thread_path(thread) + "/" + message.id.to_s
+  def cf_thread_path(thread, args = {})
+    cf_forum_path(thread.forum) + thread.slug + query_string(args)
   end
-  def cf_edit_message_path(thread, message)
-    cf_message_path(thread, message) + "/edit"
-  end
-  def new_cf_message_path(thread, message)
-    cf_message_path(thread, message) + "/new"
+  def edit_cf_thread_path(thread, args = {})
+    cf_thread_path(thread) + '/edit' + query_string(args)
   end
 
-  def cf_forum_url(forum)
-    return root_url + forum.slug
+  def cf_message_path(thread, message, args = {})
+    cf_thread_path(thread) + "/" + message.id.to_s + query_string(args)
+  end
+  def cf_edit_message_path(thread, message, args = {})
+    cf_message_path(thread, message) + "/edit" + query_string(args)
+  end
+  def new_cf_message_path(thread, message, args = {})
+    cf_message_path(thread, message) + "/new" + query_string(args)
   end
 
-  def cf_thread_url(thread)
-    cf_forum_url(thread.forum) + thread.slug
+  def cf_forum_url(forum, args = {})
+    forum = forum.slug unless forum.is_a?(String)
+    return root_url + forum + query_string(args)
   end
 
-  def cf_message_url(thread, message)
-    cf_thread_url(thread) + '/' + message.id.to_s
+  def cf_thread_url(thread, args = {})
+    cf_forum_url(thread.forum) + thread.slug + query_string(args)
+  end
+
+  def cf_message_url(thread, message, args = {})
+    cf_thread_url(thread) + '/' + message.id.to_s + query_string(args)
   end
 
 end
