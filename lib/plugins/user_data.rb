fbbf5176645798240661dc2a95d6d4d1055e0ee8
Christian Kruse <cjk@wwwtech.de>
2012-11-11 15:04:39 +0100

    migrated plugin to new structure

--- lib/plugins/user_data.rb
+++ lib/plugins/user_data.rb
@@ -1,11 +1,26 @@
+# -*- coding: utf-8 -*-
+
 class UserData < Plugin
-  def show_new_thread(thread)
+  def show_new_thread(thread, params)
+    set_vars(thread.message, params)
+  end
+
+  def show_new_message(thread, message, params)
+    set_vars(message, params)
+  end
+
+  def set_vars(msg, params)
     if user = current_user
-      thread.message.author.name = user.settings['name']
-      thread.message.author.email = user.settings['email']
-      thread.message.author.homepage = user.settings['homepage']
+      settings = ConfigManager.setting(user, current_forum)
+
+      msg.author   ||= settings['name']
+      msg.email    ||= settings['email']
+      msg.homepage ||= settings['homepage']
     end
   end
 end
 
 notification_center.register_hook(CfThreadsController::SHOW_NEW_THREAD, UserData.new(self))
+notification_center.register_hook(CfMessagesController::SHOW_NEW_MESSAGE, UserData.new(self))
+
+# eof
