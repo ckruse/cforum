6cd593f1331e1b1170ca36e992ccaf936ca4454c
Christian Kruse <cjk@defunced.de>
2012-11-26 10:40:01 +0100

    fixed tag model

--- app/models/cf_tag.rb
+++ app/models/cf_tag.rb
@@ -4,8 +4,8 @@ class CfTag < ActiveRecord::Base
   self.primary_key = 'tag_id'
   self.table_name  = 'cforum.tags'
 
-  has_many :tags_forums
-  has_many :forums, class_name: 'CfForum', :through => :tags_forums
+  has_many :tags_threads
+  has_many :threads, class_name: 'CfThread', :through => :tags_threads
 
   attr_accessible :tag_id, :tag_name
 end
