0bbddef2e1e7525338bc3783d098bec562e05703
Christian Kruse <cjk@wwwtech.de>
2012-12-06 21:15:11 +0100

    some tests for CfMessage

---
+++ test/unit/cf_message_test.rb
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+require 'test_helper'
+
+class CfMessageTest < ActiveSupport::TestCase
+  test "validations" do
+    m = CfMessage.new
+    assert !m.save
+
+    m.subject = 'Down with the Imperium!'
+    assert !m.save
+
+    m.content = Faker::Lorem.paragraphs.join("\n\n")
+    assert !m.save
+
+    m.author = 'Luke'
+    assert !m.save
+
+    t = FactoryGirl.create(:cf_thread)
+    m.thread_id = t.thread_id
+
+    assert !m.save
+
+    m.subject = nil
+    m.forum_id = t.forum_id
+    assert !m.save
+
+    m.subject = 'a'
+    assert !m.save
+
+    m.subject = 'Down with the Imperium!'
+    m.author = 'a'
+    assert !m.save
+
+    m.author = 'Luke'
+    m.content = 'a'
+    assert !m.save
+
+    m.content = Faker::Lorem.paragraphs.join("\n\n")
+    m.email = '24234'
+    assert !m.save
+
+    m.email = 'a'
+    assert !m.save
+
+    m.email = 'luke@rebellion.gov'
+    m.homepage = 'a'
+    assert !m.save
+
+    m.homepage = 'wefwef'
+    assert !m.save
+
+    m.homepage = 'http://heise.de/'
+    assert m.save
+  end
+end
+
+
+# eof
