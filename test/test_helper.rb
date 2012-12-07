6fefb6b3b60f80b26bf5609affdddadbfde2874a
Christian Kruse <cjk@wwwtech.de>
2012-12-07 13:27:47 +0100

    added Devise::TestHelpers to action controller test case

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,9 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
+# eof
