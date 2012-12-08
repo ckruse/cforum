fc7aa9298fdb39825f60cea914b34192218ece0c
Christian Kruse <cjk@wwwtech.de>
2012-12-08 21:26:50 +0100

    loads of new tests, now CfThreadsController should fully be tested

--- test/functional/cf_threads_controller_test.rb
+++ test/functional/cf_threads_controller_test.rb
@@ -391,6 +391,466 @@ class CfThreadsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:thread)
     assert_not_nil assigns(:thread).message
   end
+
+  test "create: should generate a preview in public forum" do
+    forum = FactoryGirl.create(:cf_forum, :public => true)
+
+    cnt = CfThread.count
+
+    post :create, {preview: true, curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+
+    assert_equal cnt, CfThread.count
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+  end
+
+  test "create: should create new thread in public forum" do
+    forum = FactoryGirl.create(:cf_forum, :public => true)
+
+    assert_difference('CfThread.count') do
+      assert_difference('CfMessage.count') do
+        post :create, {curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+      end
+    end
+
+    assert_not_nil flash[:notice]
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+  end
+
+  test "create: should not generate a preview in non-public forum" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+
+    catched = false
+    begin
+      post :create, {preview: true, curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "create: should not create a thread in non-public forum" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+
+    catched = false
+    begin
+      post :create, {curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "create: should generate a preview in non-public forum because of admin" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+    user = FactoryGirl.create(:cf_user, admin: true)
+
+    cnt = CfThread.count
+
+    sign_in user
+    post :create, {preview: true, curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+
+    assert_equal cnt, CfThread.count
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+  end
+
+  test "create: should create new thread in non-public forum because of admin" do
+    forum = FactoryGirl.create(:cf_forum, :public => true)
+    user  = FactoryGirl.create(:cf_user, admin: true)
+
+    sign_in user
+
+    assert_difference('CfThread.count') do
+      assert_difference('CfMessage.count') do
+        post :create, {curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+      end
+    end
+
+    assert_not_nil flash[:notice]
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+  end
+
+  test "create: should not generate a preview in non-public forum because of read permission" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+    user  = FactoryGirl.create(:cf_user, admin: false)
+    cpp   = CfForumPermission.create(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    catched = false
+    begin
+      post :create, {preview: true, curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "create: should not create new thread in non-public forum because of read permission" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+    user  = FactoryGirl.create(:cf_user, admin: false)
+    cpp   = CfForumPermission.create(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    catched = false
+    begin
+      post :create, {curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "create: should generate a preview in non-public forum because of write permission" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+    user  = FactoryGirl.create(:cf_user, admin: false)
+    cpp   = CfForumPermission.create(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    cnt = CfThread.count
+
+    sign_in user
+    post :create, {preview: true, curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+
+    assert_equal cnt, CfThread.count
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+  end
+
+  test "create: should create new thread in non-public forum because of write permission" do
+    forum = FactoryGirl.create(:cf_forum, :public => true)
+    user  = FactoryGirl.create(:cf_user, admin: false)
+    cpp   = CfForumPermission.create(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    sign_in user
+
+    assert_difference('CfThread.count') do
+      assert_difference('CfMessage.count') do
+        post :create, {curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+      end
+    end
+
+    assert_not_nil flash[:notice]
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+  end
+
+  test "create: should generate a preview in non-public forum because of moderator permission" do
+    forum = FactoryGirl.create(:cf_forum, :public => false)
+    user  = FactoryGirl.create(:cf_user, admin: false)
+    cpp   = CfForumPermission.create(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    cnt = CfThread.count
+
+    sign_in user
+    post :create, {preview: true, curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+
+    assert_equal cnt, CfThread.count
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+  end
+
+  test "create: should create new thread in non-public forum because of moderator permission" do
+    forum = FactoryGirl.create(:cf_forum, :public => true)
+    user  = FactoryGirl.create(:cf_user, admin: false)
+    cpp   = CfForumPermission.create(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    sign_in user
+
+    assert_difference('CfThread.count') do
+      assert_difference('CfMessage.count') do
+        post :create, {curr_forum: forum.slug, cf_thread: { message: {subject: 'Long live the imperator!', author: 'Anaken Skywalker', content: 'Long live the imperator! Down with the rebellion!'}}}
+      end
+    end
+
+    assert_not_nil flash[:notice]
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+  end
+
+  test "moving: should not show form because of anonymous" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+
+    catched = false
+    begin
+      get :moving, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "moving: should not show form because of permissions" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :moving, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "moving: should not show form because of read permissions" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+    cpp     = CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :moving, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "moving: should not show form because of write permissions" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+    cpp     = CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :moving, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "moving: should show form because of admin" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    sign_in user
+
+    get :moving, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug}
+
+    assert_response :success
+    assert_not_nil assigns(:forums)
+    assert_not_nil assigns(:thread)
+  end
+
+  test "moving: should show form because of moderator permission" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+    cpp     = CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    sign_in user
+
+    get :moving, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug}
+
+    assert_response :success
+    assert_not_nil assigns(:forums)
+    assert_not_nil assigns(:thread)
+  end
+
+
+  test "move: should not move because of anonymous" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+
+
+    catched = false
+    begin
+      post :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "move: should not move because of authorization" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "move: should not move because of read permission only in forum" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "move: should not move because of read permissions" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+    CfForumPermission.create!(forum_id: forum1.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "move: should not move because of write permission only in forum" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "move: should not move because of write permissions" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+    CfForumPermission.create!(forum_id: forum1.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    sign_in user
+
+    catched = false
+    begin
+      get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+    rescue CForum::ForbiddenException
+      catched = true
+    end
+    assert catched
+  end
+
+  test "move: should move because of admin" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    cnt_forum  = forum.threads.count
+    cnt_forum1 = forum1.threads.count
+
+    sign_in user
+
+    assert_difference lambda { forum.threads.count }, -1 do
+      assert_difference lambda { forum1.threads.count }, +1 do
+        get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+      end
+    end
+
+    assert_not_nil assigns(:thread)
+    assert_not_nil flash[:notice]
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:thread).message)
+  end
+
+  test "move: should move because of moderator" do
+    forum   = FactoryGirl.create(:cf_forum, :public => true)
+    forum1  = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, slug: '/2012/dec/6/star-wars', forum: forum)
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+    CfForumPermission.create!(forum_id: forum1.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    cnt_forum  = forum.threads.count
+    cnt_forum1 = forum1.threads.count
+
+    sign_in user
+
+    assert_difference lambda { forum.threads.count }, -1 do
+      assert_difference lambda { forum1.threads.count }, +1 do
+        get :move, {year: '2012', mon: 'dec', day: '6', tid: 'star-wars', curr_forum: thread.forum.slug, move_to: forum1.forum_id}
+      end
+    end
+
+    assert_not_nil assigns(:thread)
+    assert_not_nil flash[:notice]
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:thread).message)
+  end
+
 end
 
 # eof
