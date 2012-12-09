87676145043b90a66dec0629e1d039040504ed20
Christian Kruse <cjk@wwwtech.de>
2012-12-09 02:19:49 +0100

    loads of more tests; CfMessagesController should be completely tested

--- test/functional/cf_messages_controller_test.rb
+++ test/functional/cf_messages_controller_test.rb
@@ -25,6 +25,121 @@ class CfMessagesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:thread)
   end
 
+  test "show: should not show deleted message because of anonymous" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+
+    assert_raise(CForum::NotFoundException) do
+      get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id, view_all: true}
+    end
+  end
+
+  test "show: should not show deleted message because of permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    sign_in user
+
+    assert_raise(CForum::NotFoundException) do
+      get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id, view_all: true}
+    end
+  end
+
+  test "show: should not show deleted message because of admin and not view_all" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    sign_in user
+
+    assert_raise(CForum::NotFoundException) do
+      get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "show: should show deleted message because of admin and view_all" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    sign_in user
+
+    get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id, view_all: true}
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+    assert assigns(:message).deleted
+  end
+
+  test "show: should not show deleted message because of read permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    assert_raise(CForum::NotFoundException) do
+      get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id, view_all: true}
+    end
+  end
+
+  test "show: should not show deleted message because of write permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    sign_in user
+
+    assert_raise(CForum::NotFoundException) do
+      get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id, view_all: true}
+    end
+  end
+
+  test "show: should not show deleted message because of moderator permissions and not view_all" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    sign_in user
+
+    assert_raise(CForum::NotFoundException) do
+      get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "show: should show deleted message because of moderator permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    sign_in user
+
+    get :show, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id, view_all: true}
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+    assert assigns(:message).deleted
+  end
+
   test "show: should fail in private forum because of anonymous" do
     forum   = FactoryGirl.create(:cf_forum, :public => false)
     thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
@@ -451,6 +566,184 @@ class CfMessagesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:parent)
     assert_not_nil assigns(:thread)
     assert_not_nil assigns(:preview)
+  end
+
+  test "destroy: should not destroy because of anonymous" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+
+    assert_raise(CForum::ForbiddenException) do
+      delete :destroy, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "destroy: should not destroy because of permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    sign_in user
+
+    assert_raise(CForum::ForbiddenException) do
+      delete :destroy, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "destroy: should destroy because of admin" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    sign_in user
+
+    delete :destroy, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+    assert assigns(:message).deleted
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message), view_all: 'true')
+  end
+
+  test "destroy: should not destroy because of read permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    assert_raise(CForum::ForbiddenException) do
+      delete :destroy, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "destroy: should not destroy because of write permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    sign_in user
+
+    assert_raise(CForum::ForbiddenException) do
+      delete :destroy, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "destroy: should destroy because of moderator permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    sign_in user
+    delete :destroy, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+    assert assigns(:message).deleted
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message), view_all: 'true')
+  end
+
+
+  test "restore: should not restore because of anonymous" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+
+    assert_raise(CForum::ForbiddenException) do
+      post :restore, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "restore: should not restore because of permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    sign_in user
+
+    assert_raise(CForum::ForbiddenException) do
+      post :restore, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "restore: should restore because of admin" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: true)
+
+    sign_in user
+
+    post :restore, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+    assert !assigns(:message).deleted
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message), view_all: 'true')
+  end
+
+  test "restore: should not restore because of read permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_READ)
+
+    sign_in user
+
+    assert_raise(CForum::ForbiddenException) do
+      post :restore, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "restore: should not restore because of write permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_WRITE)
+
+    sign_in user
+
+    assert_raise(CForum::ForbiddenException) do
+      post :restore, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+    end
+  end
+
+  test "restore: should restore because of moderator permissions" do
+    forum   = FactoryGirl.create(:cf_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread, deleted: true)
+    user    = FactoryGirl.create(:cf_user, admin: false)
+
+    CfForumPermission.create!(forum_id: forum.forum_id, user_id: user.user_id, permission: CfForumPermission::ACCESS_MODERATOR)
+
+    sign_in user
+
+    post :restore, {curr_forum: forum.slug, year: '2012', mon: 'dec', day: '6', tid: 'obi-wan-kenobi', mid: message.message_id}
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+    assert !assigns(:message).deleted
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message), view_all: 'true')
   end
 
 end
