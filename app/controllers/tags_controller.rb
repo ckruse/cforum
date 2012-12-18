9fb5a7b476a537b5ec5855156517073d78cb85c0
Christian Kruse <cjk@defunct.ch>
2012-12-18 17:05:01 +0100

    implemented tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -27,7 +27,16 @@ class TagsController < ApplicationController
   # GET /collections/1
   # GET /collections/1.json
   def show
-    @tag = Tag.find(params[:id])
+    per_page = 100
+    @tag = CfTag.where('tags.forum_id = ? AND slug = ?', current_forum.forum_id, params[:id]).first
+
+    page = params[:p].to_i
+    page = 0 if page < 0
+    page = (@tag.num_threads / per_page).ceil if page > (@tag.num_threads / per_page).ceil
+
+    offset = page * per_page
+
+    @threads = CfThread.preload(:messages => :owner).includes(:tags_threads).where('tags_threads.tag_id' => @tag.tag_id, deleted: false).order('threads.created_at DESC').limit(per_page).offset(offset).all
 
     respond_to do |format|
       format.html # show.html.erb
@@ -35,65 +44,6 @@ class TagsController < ApplicationController
     end
   end
 
-  # GET /collections/new
-  # GET /collections/new.json
-  def new
-    @tag = Tag.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @tag }
-    end
-  end
-
-  # GET /collections/1/edit
-  def edit
-    @tag = Tag.find(params[:id])
-  end
-
-  # POST /collections
-  # POST /collections.json
-  def create
-    @tag = Tag.new(params[:tag])
-
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render json: @tag, status: :created, location: @tag }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /collections/1
-  # PUT /collections/1.json
-  def update
-    @tag = Tag.find(params[:id])
-
-    respond_to do |format|
-      if @tag.update_attributes(params[:tag])
-        format.html { redirect_to @tag, notice: 'Tag was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /collections/1
-  # DELETE /collections/1.json
-  def destroy
-    @tag = Tag.find(params[:id])
-    @tag.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tags_url }
-      format.json { head :no_content }
-    end
-  end
 end
 
 # eof
--- app/models/cf_tag.rb
+++ app/models/cf_tag.rb
@@ -8,10 +8,18 @@ class CfTag < ActiveRecord::Base
   has_many :threads, class_name: 'CfThread', :through => :tags_threads
   belongs_to :forum, class_name: 'CfForum', :foreign_key => :forum_id
 
-  attr_accessible :tag_id, :tag_name, :forum_id
+  attr_accessible :tag_id, :tag_name, :forum_id, :slug
 
   validates_presence_of :tag_name, :forum_id
   validates :tag_name, length: {:in => 2..50}
+
+  def to_param
+    slug
+  end
+
+  before_create do |t|
+    t.slug = t.tag_name.parameterize
+  end
 end
 
 # eof
