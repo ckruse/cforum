019a5ee38b8ac611512d17ba6724573d50f6a24e
Christian Kruse <cjk@wwwtech.de>
2012-12-26 20:00:03 +0100

    moved archiver to correct namespace and created some better inform log entries

--- lib/async/archive_runner_task.rb
+++ lib/async/archive_runner_task.rb
@@ -1,53 +1,52 @@
 # -*- coding: utf-8 -*-
 
-class ArchiveRunnerTask
-  def initialize(cmanager)
-    @config_manager = cmanager
-  end
+module Peon
+  module Tasks
+    class ArchiveRunnerTask < PeonTask
+      def archive_forum(forum)
+        Rails.logger.info "ArchiveRunnerTask: running archiver for forum #{forum.name}"
 
-  def conf(name, forum, default = nil)
-    @config_manager.get(name, default, nil, forum)
-  end
+        max_threads  = conf('max_threads', forum, '150').to_i # max threads per forum
+        max_messages = conf('max_messages_per_thread', forum, '50').to_i # max messages per thread
 
-  def archive_forum(forum)
-    Rails.logger.info "Running archiver for forum #{forum.name}"
+        # first: max messages per thread (to avoid monster threads like „Test, bitte ignorieren”)
+        CfThread.transaction do
+          threads = CfThread.select('threads.thread_id, COUNT(*) AS cnt').joins(:messages).where(archived: false, forum_id: forum.forum_id).group('threads.thread_id')
 
-    max_threads  = conf('max_threads', forum, '150').to_i # max threads per forum
-    max_messages = conf('max_messages_per_thread', forum, '50').to_i # max messages per thread
+          threads.each do |t|
+            if t.cnt.to_i > max_messages
+              Rails.logger.info 'ArchiveRunnerTask: archiving thread ' + t.thread_id.to_s + ' because of to many messages'
+              CfThread.connection.execute 'UPDATE threads SET archived = true WHERE thread_id = ' + t.thread_id.to_s
+            end
+          end
+        end
 
-    # first: max messages per thread (to avoid monster threads like „Test, bitte ignorieren”)
-    CfThread.transaction do
-      threads = CfThread.select('threads.thread_id, COUNT(*) AS cnt').joins(:messages).where(archived: false, forum_id: forum.forum_id).group('threads.thread_id')
 
-      threads.each do |t|
-        CfThread.connection.execute 'UPDATE threads SET archived = true WHERE thread_id = ' + t.thread_id.to_s if t.cnt.to_i > max_messages
+        # second: max threads per forum
+        CfThread.transaction do
+          while CfThread.where(forum_id: forum.forum_id, archived: false).count > max_threads
+            rslt = CfThread.connection.execute 'SELECT threads.thread_id, MAX(messages.created_at) AS created_at FROM threads INNER JOIN cforum.messages USING(thread_id) WHERE threads.forum_id = ' + forum.forum_id.to_s + ' AND archived = false GROUP BY threads.thread_id ORDER BY MAX(messages.created_at) ASC LIMIT 1'
+            tid = rslt[0]['thread_id']
+
+            Rails.logger.info 'ArchiveRunnerTask: archiving thread ' + tid + ' because oldest while to many threads'
+            CfThread.connection.execute 'UPDATE threads SET archived = true WHERE thread_id = ' + tid
+          end
+        end
+      end
+
+
+      def work_work(args)
+        # run for each forum separately
+        forums = CfForum.all
+
+        forums.each do |f|
+          archive_forum(f) if conf('use_archive', f, 'no') == 'yes'
+        end
       end
     end
 
-
-    # second: max threads per forum
-    CfThread.transaction do
-      while CfThread.where(forum_id: forum.forum_id, archived: false).count > max_threads
-        rslt = CfThread.connection.execute 'SELECT threads.thread_id, MAX(messages.created_at) AS created_at FROM threads INNER JOIN cforum.messages USING(thread_id) WHERE threads.forum_id = ' + forum.forum_id.to_s + ' AND archived = false GROUP BY threads.thread_id ORDER BY MAX(messages.created_at) ASC LIMIT 1'
-        tid = rslt[0]['thread_id']
-
-        CfThread.connection.execute 'UPDATE threads SET archived = true WHERE thread_id = ' + tid
-      end
-    end
-  end
-
-
-  def work_work(args)
-    # run for each forum separately
-    forums = CfForum.all
-
-    forums.each do |f|
-      archive_forum(f) if conf('use_archive', f, 'no') == 'yes'
-    end
+    Peon::Grunt.instance.periodical(ArchiveRunnerTask.new, 120)
   end
 end
-
-grunt = Peon::Grunt.instance
-grunt.periodical(ArchiveRunnerTask.new(grunt.config_manager), 120)
 
 # eof
