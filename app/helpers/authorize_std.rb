5b4b2abb4781d04f3c6899111e09f67eb3c57fe4
Christian Kruse <cjk@defunct.ch>
2013-01-11 01:15:03 +0100

    fixed standard authorization

--- app/helpers/authorize_std.rb
+++ app/helpers/authorize_std.rb
@@ -10,15 +10,18 @@ module AuthorizeStd
     return if user and user.admin
 
     unless user.blank?
-      user.rights.each do |r|
-        if r.forum_id == forum.forum_id
-          if %w{new edit create update destroy}.include?(action_name)
-            return if %w{moderator write}.include?(r.permission)
-          else
-            return if %w{moderator read write}.include?(r.permission)
-          end
+      permissions = CfForumGroupPermission
+        .where("group_id IN (SELECT group_id FROM users_groups WHERE user_id = ?) AND forum_id = ?", user.user_id, forum.forum_id)
+        .all
+
+      permissions.each do |p|
+        if %w{new edit create update destroy}.include?(action_name)
+          return if %w{moderator write}.include?(p.permission)
+        else
+          return if %w{moderator read write}.include?(p.permission)
         end
       end
+
     end
 
     raise CForum::ForbiddenException.new
