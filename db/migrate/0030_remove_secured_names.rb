39040091c2808479434c742d9b3ba980372a8bd0
Christian Kruse <cjk@defunct.ch>
2013-01-14 17:28:47 +0100

    removed no longer needed files

---
+++ db/migrate/0030_remove_secured_names.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+class RemoveSecuredNames < ActiveRecord::Migration
+  def up
+    drop_table 'secured_names'
+  end
+
+  def down
+    execute %q{
+CREATE TABLE secured_names (
+  user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE PRIMARY KEY,
+  name CHARACTER VARYING NOT NULL
+);
+
+CREATE UNIQUE INDEX secured_names_lower_name_idx ON secured_names (LOWER(name));
+    }
+  end
+end
+
+# eof
--- lib/plugins/controllers/user_data/user_data.rb
+++ lib/plugins/controllers/user_data/user_data.rb
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require File.join(File.dirname(__FILE__), 'secured_name.rb')
-
 class UserDataPlugin < Plugin
   def show_new_thread(thread)
     set_vars(thread.message)
@@ -13,7 +11,6 @@ class UserDataPlugin < Plugin
 
   def set_vars(msg)
     if user = current_user
-      msg.author   ||= user.conf('name')
       msg.email    ||= user.conf('email')
       msg.homepage ||= user.conf('url')
 
@@ -29,69 +26,12 @@ class UserDataPlugin < Plugin
     end
   end
 
-  def saving_settings(user, settings)
-    if settings.options['secured_name'] == 'yes' and not settings.options['name'].blank?
-      nam = settings.options['name'].strip.downcase
-
-      sn = SecuredName.where('name = LOWER(?)', nam).first
-      existing = SecuredName.find_by_user_id user.user_id
-
-      if sn.blank?
-        SecuredName.transaction do
-          if existing
-            existing.name = nam
-            existing.save
-          else
-            SecuredName.create!(:user_id => user.user_id, name: nam)
-          end
-        end
-
-        return
-
-      elsif sn.user_id == user.user_id
-        return
-
-      else
-        flash[:error] = I18n.t('plugins.user_data.nick_is_secured')
-      end
-
-      raise ActiveRecord::Rollback.new
-
-    elsif user.conf('secured_name') == 'yes'
-      sn = SecuredName.find_by_user_id user.user_id
-      sn.destroy unless sn.blank?
-
-    end
-
-  end
-
-  def creating_new_message(thread, parent, message)
-    unless message.author.blank?
-      if sn = SecuredName.where('name = LOWER(?)', message.author).first
-        return true if current_user and sn.user_id == current_user.user_id
-
-        flash[:error] = I18n.t('plugins.user_data.nick_is_secured')
-        return false
-      end
-
-    end
-  end
-
-  def new_thread(thread, message, tags)
-    creating_new_message(thread, nil, message)
-  end
-
 end
 
 ApplicationController.init_hooks << Proc.new do |app_controller|
   ud_plugin = UserDataPlugin.new(app_controller)
   app_controller.notification_center.register_hook(CfThreadsController::SHOW_NEW_THREAD, ud_plugin)
   app_controller.notification_center.register_hook(CfMessagesController::SHOW_NEW_MESSAGE, ud_plugin)
-
-  app_controller.notification_center.register_hook(UsersController::SAVING_SETTINGS, ud_plugin)
-
-  app_controller.notification_center.register_hook(CfMessagesController::CREATING_NEW_MESSAGE, ud_plugin)
-  app_controller.notification_center.register_hook(CfThreadsController::NEW_THREAD, ud_plugin)
 end
 
 # eof
