a2a94c27761cb2110262ddaa744fd07bcdd3200f
ckruse <cjk@defunct.ch>
2013-01-14 18:47:05 +0100

    added paging for notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,7 +6,16 @@ class NotificationsController < ApplicationController
   include AuthorizeUser
 
   def index
-    @notifications = CfNotification.where(recipient_id: current_user.user_id).order('created_at DESC').limit(10).all
+    @page  = params[:p].to_i
+    @limit = uconf('pagination', 50).to_i
+
+    @page  = 0 if @page < 0
+    @limit = 50 if @limit <= 0
+
+    @limit = 3
+
+    @notifications = CfNotification.where(recipient_id: current_user.user_id).order('created_at DESC').limit(@limit).offset(@page * @limit).all
+    @all_notifications_count = CfNotification.where(recipient_id: current_user.user_id).count
 
     respond_to do |format|
       format.html
