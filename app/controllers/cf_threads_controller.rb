a39b9b465192ab12d6e80ca9278d4c72f743c647
ckruse <cjk@defunct.ch>
2013-01-14 23:14:52 +0100

    fix: admins may always see all forums

--- app/controllers/cf_threads_controller.rb
+++ app/controllers/cf_threads_controller.rb
@@ -36,17 +36,22 @@ class CfThreadsController < ApplicationController
         crits << "EXISTS(SELECT message_id FROM messages WHERE thread_id = threads.thread_id AND deleted = false)"
       end
     else
-      crits << "forum_id IN (SELECT forum_id FROM forums_groups_permissions INNER JOIN groups_users USING(group_id) WHERE user_id = " + current_user.user_id.to_s + ")" if current_user
-      crits << "forum_id IN (SELECT forum_id FROM forums WHERE standard_permission IN ('" +
-        CfForumGroupPermission::ACCESS_READ + "','" +
-        CfForumGroupPermission::ACCESS_WRITE +
-          (current_user ? ("', '" +
-                           CfForumGroupPermission::ACCESS_KNOWN_WRITE + "','" +
-                           CfForumGroupPermission::ACCESS_KNOWN_READ) : ""
-          ) +
-        "'))"
+      unless current_user.admin?
+        crits << "forum_id IN (SELECT forum_id FROM forums_groups_permissions INNER JOIN groups_users USING(group_id) WHERE user_id = " + current_user.user_id.to_s + ")" if current_user
+        crits << "forum_id IN (SELECT forum_id FROM forums WHERE standard_permission IN ('" +
+          CfForumGroupPermission::ACCESS_READ + "','" +
+          CfForumGroupPermission::ACCESS_WRITE +
+            (current_user ? ("', '" +
+                             CfForumGroupPermission::ACCESS_KNOWN_WRITE + "','" +
+                             CfForumGroupPermission::ACCESS_KNOWN_READ) : ""
+            ) +
+          "'))"
 
-      crits = ["(" + crits.join(" OR ") + ")"]
+        crits = ["(" + crits.join(" OR ") + ")"]
+
+      else
+        crits = []
+      end
 
       unless @view_all
         crits << "EXISTS(SELECT message_id FROM messages WHERE thread_id = threads.thread_id AND deleted = false)"
