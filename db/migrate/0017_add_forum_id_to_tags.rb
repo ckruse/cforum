2571d4188c31b81573045261f7a3b9f984593588
Christian Kruse <cjk@wwwtech.de>
2013-01-19 00:32:59 +0100

    keep old models so that migrations still work

--- db/migrate/0017_add_forum_id_to_tags.rb
+++ db/migrate/0017_add_forum_id_to_tags.rb
@@ -1,5 +1,39 @@
 # -*- coding: utf-8 -*-
 
+class CfTag < ActiveRecord::Base
+  self.primary_key = 'tag_id'
+  self.table_name  = 'tags'
+
+  has_many :tags_threads, class_name: 'CfTagThread', :foreign_key => :tag_id, :dependent => :destroy
+  has_many :threads, class_name: 'CfThread', :through => :tags_threads
+  belongs_to :forum, class_name: 'CfForum', :foreign_key => :forum_id
+
+  attr_accessible :tag_id, :tag_name, :forum_id, :slug
+
+  validates_presence_of :tag_name, :forum_id
+  validates :tag_name, length: {:in => 2..50}
+
+  def to_param
+    slug
+  end
+
+  before_create do |t|
+    t.slug = t.tag_name.parameterize
+  end
+end
+
+class CfTagThread < ActiveRecord::Base
+  self.primary_key = 'tag_thread_id'
+  self.table_name  = 'tags_threads'
+
+  belongs_to :thread, class_name: 'CfThread', :foreign_key => :thread_id
+  belongs_to :tag, class_name: 'CfTag', :foreign_key => :tag_id
+
+  attr_accessible :tag_thread_id, :tag_id, :thread_id
+
+  validates_presence_of :tag_id, :thread_id
+end
+
 class AddForumIdToTags < ActiveRecord::Migration
   def up
     execute %q{
