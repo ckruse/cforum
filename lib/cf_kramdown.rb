c058533086b775bf7e6c7ecdb63c4605881fe68f
ckruse <cjk@defunct.ch>
2013-02-16 00:10:52 +0100

    implemented email style signatures

--- lib/cf_kramdown.rb
+++ lib/cf_kramdown.rb
@@ -13,7 +13,26 @@ class Kramdown::Parser::CfMarkdown < Kramdown::Parser::Kramdown
     @block_parsers.delete :block_html
     @span_parsers.delete :span_html
     @span_parsers.delete :html_entity
+
+    @block_parsers.unshift :email_style_sig
+    @span_parsers.unshift :email_style_sig
   end
+
+  CF_SETEXT_HEADER_START = /^(#{OPT_SPACE}[^ \t].*?)#{HEADER_ID}[ \t]*?\n(-|=)+\n/
+  @@parsers.delete(:setext_header)
+  define_parser(:setext_header, CF_SETEXT_HEADER_START)
+
+  SIGNATURE_START = /^-- \n/
+  def parse_email_style_sig
+    @src.pos += @src.matched_size
+    el = new_block_el(:email_style_sig)
+
+    @tree.children << el
+    parse_spans(el)
+
+    true
+  end
+  define_parser(:email_style_sig, SIGNATURE_START) unless @@parsers.has_key?(:email_style_sig)
 end
 
 class Kramdown::Converter::CfHtml < Kramdown::Converter::Html
@@ -22,6 +41,10 @@ class Kramdown::Converter::CfHtml < Kramdown::Converter::Html
 
     ret.gsub! /^<div>(.*)<\/div>/m, '\1'
     '<code><pre>' + ret + '</pre></code>'
+  end
+
+  def convert_email_style_sig(el, indent)
+    "<span class=\"signature\"><br />\n-- <br />\n" + inner(el, indent) + "</span>"
   end
 
 end
