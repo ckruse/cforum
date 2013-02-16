a3d5432de50c67767fa009c5da6d750c3c45b729
Christian Kruse <cjk@wwwtech.de>
2013-02-16 18:12:15 +0100

    added a cache to avoid too many queries

--- lib/plugins/controllers/mark_read/mark_read.rb
+++ lib/plugins/controllers/mark_read/mark_read.rb
@@ -4,6 +4,8 @@ class MarkReadPlugin < Plugin
   def initialize(*args)
     super(*args)
 
+    @cache = {}
+
     register_plugin_api :mark_read do |message, user|
       mark_read(message, user)
     end
@@ -14,17 +16,41 @@ class MarkReadPlugin < Plugin
 
   def is_read(message, user)
     return if user.blank?
+
     message = [message] unless message.is_a?(Array)
     message = message.map {|m| m.is_a?(CfMessage) ? m.message_id : m.to_i}
 
     user_id = user.is_a?(CfUser) ? user.user_id : user
 
+    new_cache = {}
+
+    if @cache[user_id]
+      has_all = true
+      retval = []
+
+      message.each do |m|
+        if not @cache[user_id].has_key?(m)
+          has_all = false
+        else
+          retval << m if @cache[user_id][m]
+        end
+        new_cache[m] = false
+      end
+
+      return retval if has_all
+    end
+
     read_messages = []
 
     result = CfMessage.connection.execute("SELECT message_id FROM read_messages WHERE message_id IN (" + message.join(", ") + ") AND user_id = " + user_id.to_s)
     result.each do |row|
-      read_messages << row['message_id'].to_i
+      m = row['message_id'].to_i
+      read_messages << m
+      new_cache[m] = true
     end
+
+    @cache[user_id] ||= {}
+    @cache[user_id] = @cache[user_id].merge(new_cache)
 
     read_messages
   end
@@ -51,11 +77,14 @@ class MarkReadPlugin < Plugin
     ids = []
     msgs = {}
 
+    new_cache = {}
+
     threads.each do |t|
       num_msgs = 0
 
       t.messages.each do |m|
         ids << m.message_id
+        new_cache[m.message_id] = false
         msgs[m.message_id.to_s] = [m, t]
         num_msgs += 1
       end
@@ -66,12 +95,17 @@ class MarkReadPlugin < Plugin
     if not ids.blank?
       result = CfMessage.connection.execute("SELECT message_id FROM read_messages WHERE message_id IN (" + ids.join(", ") + ") AND user_id = " + current_user.user_id.to_s)
       result.each do |row|
+        new_cache[row['message_id'].to_i] = true
+
         if msgs[row['message_id']]
           msgs[row['message_id']][0].attribs['classes'] << 'visited'
           msgs[row['message_id']][1].attribs[:msgs][:unread] -= 1
         end
       end
     end
+
+    @cache[current_user.user_id] ||= {}
+    @cache[current_user.user_id] = @cache[current_user.user_id].merge(new_cache)
   end
 
   def show_thread(thread, message = nil)
