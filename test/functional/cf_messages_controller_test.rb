afee87c797853b871589d314d2ae1b506f32b0f6
ckruse <cjk@defunct.ch>
2013-03-12 23:09:51 +0100

    tests for messages controller

--- test/functional/cf_messages_controller_test.rb
+++ test/functional/cf_messages_controller_test.rb
@@ -1268,6 +1268,197 @@ class CfMessagesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:thread)
   end
 
+  test "should not post when too many tags are used" do
+    forum   = FactoryGirl.create(:cf_write_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    tag     = FactoryGirl.create(:cf_tag, forum: forum)
+    CfSetting.create!(options: {'max_tags_per_message' => 1})
+
+    user = FactoryGirl.create(:cf_user)
+    sign_in user
+
+    assert_no_difference 'CfMessage.count' do
+      post :create, {
+        curr_forum: forum.slug,
+        year: '2012',
+        mon: 'dec',
+        day: '6',
+        tid: 'obi-wan-kenobi',
+        mid: message.message_id.to_s,
+        cf_message: {
+          subject: 'Fighters of the world',
+          content: 'Long live the imperator! Down with the rebellion!'
+        },
+        tags: [
+          'tag1', 'tag2', 'tag3'
+        ]
+      }
+    end
+
+    assert_response :success
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:parent)
+    assert_not_nil assigns(:thread)
+  end
+
+  test "should post with not too many tags" do
+    forum   = FactoryGirl.create(:cf_write_forum)
+    thread  = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    tag     = FactoryGirl.create(:cf_tag, forum: forum)
+    CfSetting.create!(options: {'max_tags_per_message' => 4})
+
+    user = FactoryGirl.create(:cf_user)
+    sign_in user
+
+    assert_difference 'CfMessage.count' do
+      post :create, {
+        curr_forum: forum.slug,
+        year: '2012',
+        mon: 'dec',
+        day: '6',
+        tid: 'obi-wan-kenobi',
+        mid: message.message_id.to_s,
+        cf_message: {
+          subject: 'Fighters of the world',
+          content: 'Long live the imperator! Down with the rebellion!'
+        },
+        tags: [
+          'tag1', 'tag2', 'tag3'
+        ]
+      }
+    end
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:parent)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+  end
+
+  test "should not accept as anonymous" do
+    forum    = FactoryGirl.create(:cf_write_forum)
+    thread   = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message  = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    message1 = FactoryGirl.create(:cf_message, forum: forum, thread: thread, parent_id: message.message_id)
+
+    assert_no_difference 'CfScore.count' do
+      post :accept, {
+        curr_forum: forum.slug,
+        year: '2012',
+        mon: 'dec',
+        day: '6',
+        tid: 'obi-wan-kenobi',
+        mid: message1.message_id.to_s
+      }
+    end
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+
+    message1.reload
+    assert !message1.accepted?
+  end
+
+  test "should accept as OP owner" do
+    forum    = FactoryGirl.create(:cf_write_forum)
+    thread   = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message  = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    message1 = FactoryGirl.create(:cf_message, forum: forum, thread: thread, parent_id: message.message_id)
+
+    sign_in message.owner
+
+    assert_difference 'CfScore.count' do
+      post :accept, {
+        curr_forum: forum.slug,
+        year: '2012',
+        mon: 'dec',
+        day: '6',
+        tid: 'obi-wan-kenobi',
+        mid: message1.message_id.to_s
+      }
+    end
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+
+    message1.reload
+    assert message1.accepted?
+  end
+
+  test "should unaccept on duplicate" do
+    forum    = FactoryGirl.create(:cf_write_forum)
+    thread   = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message  = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    message1 = FactoryGirl.create(:cf_message, forum: forum, thread: thread, parent_id: message.message_id, accepted: true)
+
+    CfScore.create!(message_id: message1.message_id, value: 10, user_id: message1.user_id)
+
+    sign_in message.owner
+
+    assert_difference 'CfScore.count', -1 do
+      post :accept, {
+        curr_forum: forum.slug,
+        year: '2012',
+        mon: 'dec',
+        day: '6',
+        tid: 'obi-wan-kenobi',
+        mid: message1.message_id.to_s
+      }
+    end
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+
+    message1.reload
+    assert !message1.accepted?
+  end
+
+  test "should remove accept on first when accepting second" do
+    forum    = FactoryGirl.create(:cf_write_forum)
+    thread   = FactoryGirl.create(:cf_thread, forum: forum, slug: '/2012/dec/6/obi-wan-kenobi')
+    message  = FactoryGirl.create(:cf_message, forum: forum, thread: thread)
+    message1 = FactoryGirl.create(:cf_message, forum: forum, thread: thread, parent_id: message.message_id, accepted: true)
+    message2 = FactoryGirl.create(:cf_message, forum: forum, thread: thread, parent_id: message.message_id, accepted: false)
+
+    CfScore.create!(message_id: message1.message_id, value: 10, user_id: message1.user_id)
+
+    sign_in message.owner
+
+    assert_no_difference 'CfScore.count' do
+      post :accept, {
+        curr_forum: forum.slug,
+        year: '2012',
+        mon: 'dec',
+        day: '6',
+        tid: 'obi-wan-kenobi',
+        mid: message2.message_id.to_s
+      }
+    end
+
+    message1.reload
+    assert !message1.accepted?
+    message2.reload
+    assert message2.accepted?
+
+    score = CfScore.find_by_message_id_and_user_id(message1.message_id, message1.user_id)
+    assert_nil score
+    score = CfScore.find_by_message_id_and_user_id(message2.message_id, message2.user_id)
+    assert_not_nil score
+
+    assert_not_nil assigns(:message)
+    assert_not_nil assigns(:thread)
+
+    assert_redirected_to cf_message_url(assigns(:thread), assigns(:message))
+  end
+
 end
 
 # eof
