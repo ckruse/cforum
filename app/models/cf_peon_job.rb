260716802184100bc34f2ef9249b0ad361b41599
ckruse <cjk@defunct.ch>
2013-03-12 16:22:39 +0100

    added some validations to avoid „invalid statement“ exceptions

--- app/models/cf_peon_job.rb
+++ app/models/cf_peon_job.rb
@@ -6,6 +6,8 @@ class CfPeonJob < ActiveRecord::Base
 
   attr_accessible :peon_job_id, :queue_name, :max_tries, :tries,
     :work_done, :class_name, :arguments, :errstr, :stacktrace
+
+  validates_presence_of :queue_name, :class_name, :arguments
 end
 
 # eof
