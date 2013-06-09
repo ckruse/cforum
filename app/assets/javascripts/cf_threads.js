701b494789d2edced6cff04e37493f37bc1e8aea
Christian Kruse <cjk@wwwtech.de>
2013-06-09 14:36:32 +0200

    implemented a websockets loader for new messages and threads

--- app/assets/javascripts/cf_threads.js
+++ app/assets/javascripts/cf_threads.js
@@ -6,7 +6,29 @@ cforum.cf_threads = {
   },
   create: function() {
     cforum.tags.initTags();
+  },
+
+  index: function() {
+    var path = '/messages/' + (cforum.currentForum ? cforum.currentForum.slug : 'all');
+    cforum.client.subscribe(path, cforum.cf_threads.newMessageArriving);
+  },
+
+  newMessageArriving: function(message) {
+    $.get(
+      cforum.baseUrl + (cforum.currentForum ? cforum.currentForum.slug : '/all') + '/' + message.thread.thread_id,
+      function(data) {
+        if(message.type == 'thread') {
+          $("body [data-js=threadlist]").prepend(data);
+          $("#t" + message.thread.thread_id).addClass('new');
+        }
+        else {
+          $("[data-js=thread-" + message.thread.thread_id + "]").replaceWith(data);
+          $("#m" + message.message.message_id).addClass('new');
+        }
+      }
+    );
   }
+
 };
 
 /* eof */
