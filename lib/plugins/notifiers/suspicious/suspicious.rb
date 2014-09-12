8a0058638fcb649a1217db1fc1b213e4e2ef07c8
Christian Kruse <cjk@defunct.ch>
2014-09-13 01:51:54 +0200

    fix #116: mark all authors with characters out of latin1 as „suspicious”

---
+++ lib/plugins/notifiers/suspicious/suspicious.rb
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+class SuspiciousPoster < Plugin
+  def initialize(*args)
+    super(*args)
+  end
+
+  def show_threadlist(threads)
+    return if not current_user.blank? and uconf('mark_suspicious') == 'no'
+
+    threads.each do |t|
+      t.sorted_messages.each do |m|
+        m.attribs["classes"] << 'suspicious' if check_name(m.author)
+      end
+    end
+  end
+
+  def show_thread(thread, message = nil, votes = nil)
+    return if not current_user.blank? and uconf('mark_suspicious') == 'no'
+
+    thread.sorted_messages.each do |m|
+      m.attribs['classes'] << 'suspicious' if check_name(m.author)
+    end
+  end
+
+  def show_message(thread, message, votes)
+    return if not current_user.blank? and uconf('mark_suspicious') == 'no'
+    show_thread(thread)
+  end
+
+  private
+  def check_name(name)
+    name.each_codepoint do |cp|
+      return true if cp > 255
+    end
+
+    return false
+  end
+end
+
+ApplicationController.init_hooks << Proc.new do |app_controller|
+  suspicious_plugin = SuspiciousPoster.new(app_controller)
+
+  app_controller.notification_center.
+    register_hook(CfThreadsController::SHOW_THREADLIST, suspicious_plugin)
+  app_controller.notification_center.
+    register_hook(CfMessagesController::SHOW_THREAD, suspicious_plugin)
+  app_controller.notification_center.
+    register_hook(CfMessagesController::SHOW_MESSAGE, suspicious_plugin)
+end
+
+# eof
