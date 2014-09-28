1648566770632f3eb9ca1e52f029c79e778b66ef
Christian Kruse <cjk@defunct.ch>
2014-09-28 16:21:04 +0200

    refactored routes for better readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+
 Cforum::Application.routes.draw do
-  devise_for :users, class_name: 'CfUser', path_names: {sign_in: 'login', sign_out: 'logout'}, :skip => :registration
+  devise_for :users, class_name: 'CfUser', path_names: {sign_in: 'login',
+    sign_out: 'logout'}, skip: :registration
 
   devise_scope :user do
-    get '/users/registration/cancel' => 'devise/registrations#cancel', :as => :cancel_user_registration
-    post '/users/registration' => 'devise/registrations#create', :as => :user_registration
-    get '/users/registration' => 'devise/registrations#new', :as => :new_user_registration
+    get '/users/registration/cancel' => 'devise/registrations#cancel',
+      as: :cancel_user_registration
+    post '/users/registration' => 'devise/registrations#create',
+      as: :user_registration
+    get '/users/registration' => 'devise/registrations#new',
+      as: :new_user_registration
   end
 
 
@@ -44,94 +50,73 @@ Cforum::Application.routes.draw do
 
     get '/' => 'cf_threads#index', as: 'cf_threads'
 
+    #
     # thread urls
+    #
     post '/' => 'cf_threads#create'
     get '/new' => 'cf_threads#new', as: 'new_cf_thread'
     get '/:id' => 'cf_threads#show', as: 'show_cf_thread', id: /\d+/
     get '/:id/:mid' => 'cf_messages#show_header', id: /\d+/, mid: /\d+/
 
-    get '/:year/:mon/:day/:tid/move' => 'cf_threads#moving', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'move_cf_thread'
-    post '/:year/:mon/:day/:tid/move' => 'cf_threads#move', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/
-    post '/:year/:mon/:day/:tid/sticky' => 'cf_threads#sticky', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/
-    post '/:year/:mon/:day/:tid/no_archive' => 'no_answer_no_archive_plugin#no_archive', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'no_archive_cf_thread'
+    get '/:year/:mon/:day/:tid/move' => 'cf_threads#moving', year: /\d{4}/,
+      mon: /\w{3}/, day: /\d{1,2}/, as: 'move_cf_thread'
+    post '/:year/:mon/:day/:tid/move' => 'cf_threads#move', year: /\d{4}/,
+      mon: /\w{3}/, day: /\d{1,2}/
+    post '/:year/:mon/:day/:tid/sticky' => 'cf_threads#sticky', year: /\d{4}/,
+      mon: /\w{3}/, day: /\d{1,2}/
+    post '/:year/:mon/:day/:tid/no_archive' => 'no_answer_no_archive_plugin#no_archive',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/, as: 'no_archive_cf_thread'
 
+    #
     # message urls
-    get '/:year/:mon/:day/:tid/:mid' => 'cf_messages#show', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'cf_message'
-    get '/:year/:mon/:day/:tid/:mid/edit' => 'cf_messages#edit', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'edit_cf_message'
-    patch '/:year/:mon/:day/:tid/:mid' => 'cf_messages#update', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/
-    delete '/:year/:mon/:day/:tid/:mid' => 'cf_messages#destroy', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/
+    #
+    get '/:year/:mon/:day/:tid/:mid' => 'cf_messages#show', year: /\d{4}/,
+      mon: /\w{3}/, day: /\d{1,2}/, as: 'cf_message'
+    get '/:year/:mon/:day/:tid/:mid/edit' => 'cf_messages#edit',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/, as: 'edit_cf_message'
+    patch '/:year/:mon/:day/:tid/:mid' => 'cf_messages#update',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/
+    delete '/:year/:mon/:day/:tid/:mid' => 'cf_messages#destroy',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/
 
-    post '/:year/:mon/:day/:tid/:mid/vote' => 'vote_plugin#vote', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'vote_cf_message'
-    post '/:year/:mon/:day/:tid/:mid/restore' => 'cf_messages#restore', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'restore_cf_message'
-    post '/:year/:mon/:day/:tid/:mid/accept' => 'accept_plugin#accept', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'accept_cf_message'
-    post '/:year/:mon/:day/:tid/:mid/no_answer' => 'no_answer_no_archive_plugin#no_answer', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'no_answer_cf_message'
+    #
+    # admin actions
+    #
+    post '/:year/:mon/:day/:tid/:mid/vote' => 'vote_plugin#vote',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/, as: 'vote_cf_message'
+    post '/:year/:mon/:day/:tid/:mid/restore' => 'cf_messages#restore',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/, as: 'restore_cf_message'
+    post '/:year/:mon/:day/:tid/:mid/no_answer' => 'no_answer_no_archive_plugin#no_answer',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/, as: 'no_answer_cf_message'
 
-    get '/:year/:mon/:day/:tid/:mid/new' => 'cf_messages#new', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/, as: 'new_cf_message'
-    post '/:year/:mon/:day/:tid/:mid' => 'cf_messages#create', :year => /\d{4}/, :mon => /\w{3}/, :day => /\d{1,2}/
+    #
+    # plugins
+    #
+    post '/:year/:mon/:day/:tid/:mid/accept' => 'accept_plugin#accept',
+      year: /\d{4}/, mon: /\w{3}/, day: /\d{1,2}/, as: 'accept_cf_message'
+
+    #
+    # new and create messages
+    #
+    get '/:year/:mon/:day/:tid/:mid/new' => 'cf_messages#new', year: /\d{4}/,
+      mon: /\w{3}/, day: /\d{1,2}/, as: 'new_cf_message'
+    post '/:year/:mon/:day/:tid/:mid' => 'cf_messages#create', year: /\d{4}/,
+      mon: /\w{3}/, :day => /\d{1,2}/
   end
 
+  # old archive url
   get '/archiv/:year/:mon/:tid' => 'cf_forums#redirect_archive'
+
+  # show forum index in root
   root to: 'cf_forums#index'
 
+  # 404  handling
   if Rails.env == 'production'
     # default route to catch 404s
-    match '*a', :to => 'application#render_404', via: [:get, :post, :put, :delete, :patch]
+    match '*a', :to => 'application#render_404', via: [:get, :post, :put,
+                                                       :delete, :patch]
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
+
+# eof
