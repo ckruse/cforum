f28d58216275daf2843a0021d103123703e2848b
Christian Kruse <cjk@defunct.ch>
2014-12-26 10:53:38 +0100

    FEAT: set cursor to a nice position
    Set cursor to the first empty field or, if none empty, to the content
    textarea. Within the content textarea, set it to the first quoting
    character; we assume that in most cases you don’t need to change the
    greeting.
    
    Closes #156

--- app/assets/javascripts/cf_messages.js
+++ app/assets/javascripts/cf_messages.js
@@ -4,6 +4,15 @@
 cforum.cf_messages = {
   init: function() {
     cforum.tags.initTags();
+    cforum.cf_messages.initCursor();
+  },
+
+  initCursor: function() {
+    var content = $("#cf_message_content");
+    var subj = $("#cf_message_subject");
+    var author = $("#cf_message_author");
+
+    cforum.cf_threads.setCursor(author, subj, content);
   }
 };
 
--- app/assets/javascripts/cf_threads.js
+++ app/assets/javascripts/cf_threads.js
@@ -4,9 +4,11 @@
 cforum.cf_threads = {
   new: function() {
     cforum.tags.initTags();
+    cforum.cf_threads.initCursor();
   },
   create: function() {
     cforum.tags.initTags();
+    cforum.cf_threads.initCursor();
   },
 
   index: function() {
@@ -44,6 +46,51 @@ cforum.cf_threads = {
         $("#m" + message.message.message_id).addClass("new");
       }
     );
+  },
+
+  initCursor: function() {
+    var content = $("#cf_thread_message_content");
+    var subj = $("#cf_thread_message_subject");
+    var author = $("#cf_thread_message_author");
+
+    cforum.cf_threads.setCursor(author, subj, content);
+  },
+
+  setCursor: function(author, subject, content) {
+    if(content.length > 0) {
+      if(!subject.val()) {
+        subject.focus();
+      }
+      else {
+        if(cforum.currentUser) {
+          content.focus();
+          cforum.cf_threads.setCursorInContent(content);
+        }
+        else {
+          if(!author.val()) {
+            author.focus();
+          }
+          else {
+            content.focus();
+            cforum.cf_threads.setCursorInContent(content);
+          }
+        }
+      }
+    }
+  },
+
+  setCursorInContent: function(content) {
+    var cnt = content.val();
+    var i;
+
+    for(i = 0; i < cnt.length; ++i) {
+      if(cnt.substr(i, 1) == '>' &&
+         (i === 0 || cnt.substr(i - 1, 1) == "\n") &&
+         cnt.substr(i + 1, 1) == ' ') {
+        content.setCursorPosition(i);
+        return;
+      }
+    }
   }
 
 };
