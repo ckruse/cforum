d2e2bfe9e6d2f50cd9b9db101496b1b98135cb4d
Christian Kruse <cjk@defunct.ch>
2014-12-27 13:06:14 +0100

    FEAT: show new notifications when they arrive via JS

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -13,7 +13,29 @@ cforum.notifications = {
       }
 
     });
+  },
+
+  notifyNew: function(data) {
+    var noti = $("#notifications");
+    var txt = noti.text();
+    noti.addClass('new');
+
+    if(txt) {
+      txt = parseInt(txt);
+      txt += 1;
+      noti.text(txt);
+    }
+
+    noti.animate({'font-size': '1.3em'}, 200, function() {
+      noti.animate({'font-size': '1em'}, 200);
+    });
   }
 };
+
+$(function() {
+  if(cforum.currentUser) {
+    cforum.client.subscribe('/user/' + cforum.currentUser.user_id + "/notifications", cforum.notifications.notifyNew);
+  }
+});
 
 /* eof */
