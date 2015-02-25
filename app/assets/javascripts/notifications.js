c2ba6065ae20c537361b44fc724237a20dab41ac
Christian Kruse <cjk@defunct.ch>
2015-02-25 20:25:11 +0100

    fix: renamed IDs to avoid name clashing

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -16,7 +16,7 @@ cforum.notifications = {
   },
 
   notifyNew: function(data) {
-    var noti = $("#notifications");
+    var noti = $("#notifications-display");
     var txt = noti.text();
     noti.addClass('new');
 
