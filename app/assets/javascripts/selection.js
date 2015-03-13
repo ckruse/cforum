7a4cd9869d8048e0bd81c6e0cec7d8c297e7af6c
Christian Kruse <cjk@defunct.ch>
2015-03-13 21:13:12 +0100

    several bugfixes in tabify

--- app/assets/javascripts/selection.js
+++ app/assets/javascripts/selection.js
@@ -17,20 +17,49 @@
     )();
   };
 
+  var getSelection = function(e) {
+    return (
+      ('selectionStart' in e && function() {
+        var l = e.selectionEnd - e.selectionStart;
+        return { start: e.selectionStart, end: e.selectionEnd, length: l, text: e.value.substr(e.selectionStart, l) };
+      }) ||
+        /* browser not supported */
+      function() {
+        return null;
+      }
+    )();
+  };
+
+  var setSelection = function(e, start, end) {
+    return (
+      ('selectionStart' in e && function() {
+        e.selectionStart = start;
+        e.selectionEnd = end;
+        return;
+      }) ||
+        /* browser not supported */
+      function() {
+        return null;
+      }
+    )();
+  };
+
   $.fn.tabEnable = function() {
     this.on('keydown', function(event) {
       var insertTab = false;
 
-      if('key' in event && event.key && event.key == "@" && event.ctrlKey) {
+      if('key' in event && (event.key == "1" || event.key == "¡") && event.altKey) {
         insertTab = true;
       }
-      else if(event.keyCode == 50 && event.ctrlKey) {
+      else if(event.keyCode == 49 && event.ctrlKey) {
         insertTab = true;
       }
 
       if(insertTab) {
         event.preventDefault();
+        var sel = getSelection(this);
         replaceSelection(this, "\t");
+        setSelection(this, sel.start+1, sel.end+1);
       }
     });
   };
