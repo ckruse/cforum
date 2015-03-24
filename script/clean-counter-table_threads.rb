a51abf843f7923e215bedccf73fb7906b4b99801
Christian Kruse <cjk@defunct.ch>
2015-03-24 11:27:58 +0100

    fix: do not explicitely use the cforum schema

--- script/clean-counter-table_threads.rb
+++ script/clean-counter-table_threads.rb
@@ -7,8 +7,8 @@ require File.join(File.dirname(__FILE__), "..", "config", "environment")
 forums = CfForum.all
 forums.each do |f|
   puts "Cleaning: " + f.name
-  CfForum.connection.execute("SELECT cforum.counter_table_get_count('threads', " + f.forum_id.to_s + ")");
-  CfForum.connection.execute("SELECT cforum.counter_table_get_count('messages', " + f.forum_id.to_s + ")");
+  CfForum.connection.execute("SELECT counter_table_get_count('threads', " + f.forum_id.to_s + ")");
+  CfForum.connection.execute("SELECT counter_table_get_count('messages', " + f.forum_id.to_s + ")");
 end
 
 # eof
