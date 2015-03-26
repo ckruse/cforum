e1ca27e050f309eb7f4804350564070eaee98d5f
Christian Kruse <cjk@defunct.ch>
2015-03-26 14:45:01 +0100

    fix: do not count deleted messages

--- lib/plugins/notifiers/title_plugin/title_plugin.rb
+++ lib/plugins/notifiers/title_plugin/title_plugin.rb
@@ -19,7 +19,7 @@ class TitlePlugin < Plugin
     if uconf('show_new_messages_since_last_visit_in_title', 'no') == 'yes' and not current_user.last_sign_in_at.blank?
       cnt = CfMessage.select('count(*) AS cnt').
         joins("LEFT JOIN read_messages ON read_messages.message_id = messages.message_id AND read_messages.user_id = " + current_user.user_id.to_s).
-        where('forum_id IN (?) AND read_messages.message_id IS NULL AND messages.created_at > ?',
+        where('forum_id IN (?) AND read_messages.message_id IS NULL AND messages.created_at > ? AND deleted = false',
               @app_controller.get('forums').map { |f| f.forum_id }, current_user.last_sign_in_at).all
 
       title << cnt[0].cnt
