2f1785f0f86358cbe058abd991f26203efb5c7be
Christian Kruse <cjk@defunct.ch>
2015-03-29 11:48:18 +0200

    overhauled forum index display
    Now we don’t show archived messages in the forum index, these will be
    shown in an extra archive.

--- app/helpers/threads_helper.rb
+++ app/helpers/threads_helper.rb
@@ -9,8 +9,12 @@ module ThreadsHelper
 
     conditions = {}
     conditions[:forum_id] = forum.forum_id if forum
-    #conditions[:archived] = false if conf('use_archive') == 'yes'
-    conditions[:messages] = {deleted: false} unless @view_all
+    conditions[:archived] = false if conf('use_archive') == 'yes'
+
+    unless @view_all
+      conditions[:deleted] = false
+      conditions[:messages] = {deleted: false}
+    end
 
     order = uconf('sort_threads', 'descending')
     case order
@@ -22,21 +26,19 @@ module ThreadsHelper
       order = 'threads.created_at DESC'
     end
 
-    # the „no forum” case is much more complex; we have to do it partly manually
-    # to avoid DISTINCT
-    sql = "SELECT thread_id FROM threads "
-    crits = []
+
+    @threads = CfThread.
+               preload(:forum, messages: [:owner, :tags, votes: :voters]).
+               includes(messages: :owner).
+               where(conditions)
 
     if forum
-      crits << "forum_id = " + forum.forum_id.to_s
-      crits << "deleted = false" unless @view_all
+      @threads = @threads.where(forum_id: forum.forum_id)
     else
-      if current_user and current_user.admin?
+      if not current_user or not current_user.admin?
         crits = []
-
-      else
-        crits << "forum_id IN (SELECT forum_id FROM forums_groups_permissions INNER JOIN groups_users USING(group_id) WHERE user_id = " + current_user.user_id.to_s + ")" if current_user
-        crits << "forum_id IN (SELECT forum_id FROM forums WHERE standard_permission IN ('" +
+        crits << "threads.forum_id IN (SELECT forum_id FROM forums_groups_permissions INNER JOIN groups_users USING(group_id) WHERE user_id = " + current_user.user_id.to_s + ")" if current_user
+        crits << "threads.forum_id IN (SELECT forum_id FROM forums WHERE standard_permission IN ('" +
           CfForumGroupPermission::ACCESS_READ + "','" +
           CfForumGroupPermission::ACCESS_WRITE +
           (current_user ? ("', '" +
@@ -45,38 +47,20 @@ module ThreadsHelper
           ) +
           "'))"
 
-        crits = ["(" + crits.join(" OR ") + ")"]
-      end
-
-      unless @view_all
-        crits << 'threads.deleted = false'
+        @threads = @threads.where(crits.join(" OR "))
       end
     end
-    sql << ' WHERE ' + crits.join(" AND ") unless crits.empty?
-    sql << " ORDER BY threads.sticky DESC, #{order} LIMIT #{@limit} OFFSET #{@limit * @page}"
 
-    @threads = CfThread.
-               preload(:forum, messages: [:owner, :tags, votes: :voters]).
-               includes(messages: :owner).
-               where(conditions).
-               where("threads.thread_id IN (#{sql})").
-               order("threads.sticky DESC, #{order}")
-
-    if forum
-      rslt = CfForum.connection.execute("SELECT counter_table_get_count('threads', " +
-                                        current_forum.forum_id.to_s +
-                                        ") AS cnt")
-    else
-      rslt = CfForum.connection.execute("SELECT SUM(difference) AS cnt FROM counter_table WHERE table_name = 'threads'")
-    end
-
-    @all_threads_count = rslt[0]['cnt'].to_i
+    @threads = @threads.order("threads.sticky DESC, #{order}")
 
     ret = notification_center.notify(CfThreadsController::MODIFY_THREADLIST_QUERY_OBJ)
     ret.each do |b|
       next if b.blank?
       @threads = b.call(@threads)
     end
+
+    @all_threads_count = @threads.count
+    @threads = @threads.limit(@limit).offset(@limit * @page)
 
     @threads.each do |t|
       sort_thread(t)
