06a77b7631ef0d0943692499363e88b8f001dd07
Christian Kruse <cjk@defunct.ch>
2015-03-30 10:22:58 +0200

    refactored `index_threads` helper to be more general

--- app/helpers/threads_helper.rb
+++ app/helpers/threads_helper.rb
@@ -1,12 +1,5 @@
 module ThreadsHelper
-  def index_threads
-    forum  = current_forum
-    @page  = params[:p].to_i
-    @limit = uconf('pagination', 50).to_i
-
-    @page  = 0 if @page < 0
-    @limit = 50 if @limit <= 0
-
+  def get_threads(forum, order = 'threads.created_at DESC', user = current_user, thread_conditions = {})
     conditions = {}
     conditions[:forum_id] = forum.forum_id if forum
     conditions[:archived] = false if conf('use_archive') == 'yes'
@@ -16,34 +9,26 @@ module ThreadsHelper
       conditions[:messages] = {deleted: false}
     end
 
-    order = uconf('sort_threads', 'descending')
-    case order
-    when 'ascending'
-      order = 'threads.created_at ASC'
-    when 'newest-first'
-      order = 'threads.latest_message DESC'
-    else
-      order = 'threads.created_at DESC'
-    end
-
+    conditions.merge!(thread_conditions)
 
     @threads = CfThread.
                preload(:forum, messages: [:owner, :tags, votes: :voters]).
                includes(messages: :owner).
                where(conditions)
 
+
     if forum
       @threads = @threads.where(forum_id: forum.forum_id)
     else
-      if not current_user or not current_user.admin?
+      if not user or not user.admin?
         crits = []
-        crits << "threads.forum_id IN (SELECT forum_id FROM forums_groups_permissions INNER JOIN groups_users USING(group_id) WHERE user_id = " + current_user.user_id.to_s + ")" if current_user
+        crits << "threads.forum_id IN (SELECT forum_id FROM forums_groups_permissions INNER JOIN groups_users USING(group_id) WHERE user_id = " + user.user_id.to_s + ")" if user
         crits << "threads.forum_id IN (SELECT forum_id FROM forums WHERE standard_permission IN ('" +
           CfForumGroupPermission::ACCESS_READ + "','" +
           CfForumGroupPermission::ACCESS_WRITE +
-          (current_user ? ("', '" +
-                           CfForumGroupPermission::ACCESS_KNOWN_WRITE + "','" +
-                           CfForumGroupPermission::ACCESS_KNOWN_READ) : ""
+          (user ? ("', '" +
+                   CfForumGroupPermission::ACCESS_KNOWN_WRITE + "','" +
+                   CfForumGroupPermission::ACCESS_KNOWN_READ) : ""
           ) +
           "'))"
 
@@ -58,6 +43,29 @@ module ThreadsHelper
       next if b.blank?
       @threads = b.call(@threads)
     end
+
+    @threads
+  end
+
+  def index_threads
+    forum  = current_forum
+    @page  = params[:p].to_i
+    @limit = uconf('pagination', 50).to_i
+
+    @page  = 0 if @page < 0
+    @limit = 50 if @limit <= 0
+
+    order = uconf('sort_threads', 'descending')
+    case order
+    when 'ascending'
+      order = 'threads.created_at ASC'
+    when 'newest-first'
+      order = 'threads.latest_message DESC'
+    else
+      order = 'threads.created_at DESC'
+    end
+
+    @threads = get_threads(forum, order)
 
     @all_threads_count = @threads.count
     @threads = @threads.limit(@limit).offset(@limit * @page)
