a1ba5c14c05f9c959afdbe7f04ff26b99c42ffab
Christian Kruse <cjk@defunct.ch>
2015-04-08 17:13:24 +0200

    fix: strip markdown from text
    fixes #223

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -32,6 +32,26 @@ cforum.tags = {
     return Object.keys(words);
   },
 
+  removeLinks: function(txt) {
+    txt = txt.replace(/^([\s\t]*)([\*\-\+]|\d\.)\s+/gm, '$1')
+      .replace(/<(.*?)>/g, '$1')
+      .replace(/^[=\-]{2,}\s*$/g, '')
+      .replace(/\[\^.+?\](\: .*?$)?/g, '')
+      .replace(/\s{0,2}\[.*?\]: .*?$/g, '')
+      .replace(/\!\[.*?\][\[\(].*?[\]\)]/g, '')
+      .replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
+      .replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, '')
+      .replace(/^\#{1,6}\s*/g, '')
+      .replace(/([\*_]{1,2})(\S.*?\S)\1/g, '$2')
+      .replace(/(`{3,})(.*?)\1/gm, '$2')
+      .replace(/^-{3,}\s*$/g, '')
+      .replace(/`(.+)`/g, '$1')
+      .replace(/\n{2,}/g, '\n\n')
+      .replace(/^-- \n(.*)/m, '');
+
+    return txt;
+  },
+
   suggestTags: function() {
     var node = $("#message_input");
     var mcnt;
@@ -46,6 +66,8 @@ cforum.tags = {
     if(!mcnt) {
       return;
     }
+
+    mcnt = cforum.tags.removeLinks(mcnt);
 
     var suggestions = cforum.tags.suggestions(mcnt);
 
