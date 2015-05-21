571b07b1157349de27931a30c3cd63ec0e33faee
Christian Kruse <cjk@defunct.ch>
2015-05-21 11:45:38 +0200

    removed development mail interceptor, maybe there is a bug with it...

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'development_mail_interceptor.rb')
 require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'tools.rb')
-
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
 
 class ActionMailer::Base
   include CForum::Tools
