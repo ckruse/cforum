948a8404e277d8fc65d7643c3834235e73fcaf85
Christian Kruse <cjk@defunct.ch>
2015-05-21 15:17:52 +0200

    fix: avoid exception notification when somebody scans for vulnerabilities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   def current_forum
     if not params[:curr_forum].blank? and not params[:curr_forum] == 'all'
       @_current_forum = CfForum.find_by_slug(params[:curr_forum]) if !@_current_forum || @_current_forum.slug != params[:curr_forum]
-      raise CForum::NotFoundException.new unless @_current_forum # TODO: error description
+      raise ActiveRecord::RecordNotFound unless @_current_forum
       return @_current_forum
     end
 
