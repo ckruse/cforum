a69cb72a64c9d76049396000b1a5d026fd7bec01
Christian Kruse <cjk@defunct.ch>
2015-06-21 15:24:02 +0200

    partial spec for messages

---
+++ spec/models/cf_message_spec.rb
@@ -0,0 +1,278 @@
+require "rails_helper"
+
+describe CfMessage do
+  describe "validations" do
+    let(:thread) { create(:cf_thread) }
+
+    it "is valid with author, subject, content, forum_id and thread_id" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_valid
+    end
+
+    it "is invalid w/o author" do
+      msg = CfMessage.new(subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid w/o subject" do
+      msg = CfMessage.new(author: "Luke",
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid w/o content" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'Join the rebellion',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid w/o forum_id" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid w/o thread_id" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too short content" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'Join the rebellion',
+                          content: 'a',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too long content" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'Join the rebellion',
+                          content: 'a' * 12289,
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too short author" do
+      msg = CfMessage.new(author: "L",
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too long author" do
+      msg = CfMessage.new(author: "L" * 61,
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too short subject" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'J',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too long subject" do
+      msg = CfMessage.new(author: "Luke",
+                          subject: 'J' * 251,
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too short email" do
+      msg = CfMessage.new(author: "Luke",
+                          email: 'a@b.c',
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too long email" do
+      msg = CfMessage.new(author: "Luke",
+                          email: 'luke@rebel' + ('l' * 60) + 'lion.org',
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with wrong email syntax" do
+      msg = CfMessage.new(author: "Luke",
+                          email: 'luke the rebel',
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with invalid homepage" do
+      msg = CfMessage.new(author: "Luke",
+                          homepage: 'httpwfwef',
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+
+    it "is invalid with too long homepage" do
+      msg = CfMessage.new(author: "Luke",
+                          homepage: 'http://' + ('a' * 250) + '.de',
+                          subject: 'Join the rebellion',
+                          content: 'Join the rebellion, cookies make fat',
+                          forum_id: thread.forum_id,
+                          thread_id: thread.thread_id)
+      expect(msg).to be_invalid
+    end
+  end
+
+  describe "scoring" do
+    let(:msg) { build(:cf_message) }
+
+    it "calculates score by upvotes - downvotes" do
+      msg.upvotes = 1
+      msg.downvotes = 2
+      expect(msg.score).to eq(-1)
+    end
+
+    it "uses +/- when score is zero" do
+      expect(msg.score_str).to eq("±0")
+    end
+
+    it "uses − when score is below zero" do
+      msg.upvotes = 1
+      msg.downvotes = 2
+      expect(msg.score_str).to eq("−1")
+    end
+
+    it "uses + when score is above zero" do
+      msg.upvotes = 2
+      msg.downvotes = 1
+      expect(msg.score_str).to eq("+1")
+    end
+
+    it "returns sum of up and down votes" do
+      msg.upvotes = 2
+      msg.downvotes = 1
+      expect(msg.no_votes).to eq 3
+    end
+  end
+
+  describe "open" do
+    let(:msg) { build(:cf_message) }
+
+    it "is open when no-answer flag isn't set" do
+      expect(msg.open?).to be true
+    end
+
+    it "is open when no-answer flag is no" do
+      msg.flags['no-answer'] = 'no'
+      expect(msg.open?).to be true
+    end
+
+    it "is not open when no-answer flag is yes" do
+      msg.flags['no-answer'] = 'yes'
+      expect(msg.open?).to be false
+    end
+
+
+    it "is open when no-answer-admin flag isn't set" do
+      expect(msg.open?).to be true
+    end
+
+    it "is open when no-answer-admin flag is no" do
+      msg.flags['no-answer-admin'] = 'no'
+      expect(msg.open?).to be true
+    end
+
+    it "is not open when no-answer-admin flag is yes" do
+      msg.flags['no-answer-admin'] = 'yes'
+      expect(msg.open?).to be false
+    end
+
+
+    it "is open when no-answer flag is yes and no-answer-admin flag is no" do
+      msg.flags['no-answer'] = 'yes'
+      msg.flags['no-answer-admin'] = 'no'
+      expect(msg.open?).to be true
+    end
+
+    it "is not open when no-answer flag is no and no-answer-admin flag is yes" do
+      msg.flags['no-answer'] = 'no'
+      msg.flags['no-answer-admin'] = 'yes'
+      expect(msg.open?).to be false
+    end
+
+    it "is open when no-answer flag is no and no-answer-admin flag is no" do
+      msg.flags['no-answer'] = 'no'
+      msg.flags['no-answer-admin'] = 'no'
+      expect(msg.open?).to be true
+    end
+
+    it "is not open when no-answer flag is yes and no-answer-admin flag is yes" do
+      msg.flags['no-answer'] = 'yes'
+      msg.flags['no-answer-admin'] = 'yes'
+      expect(msg.open?).to be false
+    end
+  end
+
+  describe "subject changed" do
+    let(:thread) {
+      m = create(:cf_message)
+      m.thread
+    }
+
+    before(:each) do
+      thread.messages << build(:cf_message,
+                               thread: thread,
+                               parent_id: thread.messages.first.message_id,
+                               subject: thread.messages.first.subject)
+      thread.gen_tree
+    end
+
+    it "returns false when subject hasn't changed" do
+      expect(thread.messages.last.subject_changed?).to be false
+    end
+
+    it "returns false if message is first in tree" do
+      expect(thread.message.subject_changed?).to be false
+    end
+
+    it "returns true if subject has changed" do
+      thread.messages.last.subject = 'something different'
+      expect(thread.messages.last.subject_changed?).to be true
+    end
+  end
+end
