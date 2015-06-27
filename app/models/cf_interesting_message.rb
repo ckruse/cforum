04f3009dd59cde14249bc56de1c7ea8f0783605f
Christian Kruse <cjk@defunct.ch>
2015-06-27 14:39:12 +0200

    fix: rails version compatibility

--- app/models/cf_interesting_message.rb
+++ app/models/cf_interesting_message.rb
@@ -4,8 +4,8 @@ class CfInterestingMessage < ActiveRecord::Base
   self.primary_key = 'interesting_message_id'
   self.table_name  = 'interesting_messages'
 
-  belongs_to :message, class: CfMessage
-  belongs_to :user, class: CfUser
+  belongs_to :message, class_name: 'CfMessage'
+  belongs_to :user, class_name: 'CfUser'
 end
 
 # eof
