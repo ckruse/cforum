158a64a47f8ba1d7c56573b2600352f46038e06a
Christian Kruse <cjk@defunct.ch>
2015-06-30 16:43:10 +0200

    fix: include devise helpers in views as well

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,4 +56,5 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :view
 end
