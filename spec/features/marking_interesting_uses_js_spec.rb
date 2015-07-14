6ff48a41613f0cbe79ed39b6a77cf6f3c237ab53
Christian Kruse <cjk@defunct.ch>
2015-07-14 12:04:58 +0200

    test: wrote tests for interesting messages JS

---
+++ spec/features/marking_interesting_uses_js_spec.rb
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+require "rails_helper"
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "marking as interesting uses JS" do
+  let(:message) { create(:cf_message) }
+  let(:user) { create(:cf_user) }
+
+  before(:each) { login_as(user , scope: :user) }
+
+  include CForum::Tools
+
+  it "doesn't reload when clicking the mark interesting icon", js: true do
+    visit cf_forum_path(message.forum)
+
+    page.find('#m' + message.message_id.to_s + ' .mark-interesting').click
+    sleep 0.1
+    expect(page.body).to have_css('#m' + message.message_id.to_s + ' .mark-boring')
+    expect(CfInterestingMessage.where(user_id: user.user_id,
+                                      message_id: message.message_id).first).not_to be_nil
+
+    page.find('#m' + message.message_id.to_s + ' .mark-boring').click
+    sleep 0.1
+    expect(page.body).to have_css('#m' + message.message_id.to_s + ' .mark-interesting')
+    expect(CfInterestingMessage.where(user_id: user.user_id,
+                                      message_id: message.message_id).first).to be_nil
+  end
+
+end
+
+# eof
