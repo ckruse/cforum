42196c7b917bfeeefd352c250503bcabc49cd51a
Christian Kruse <cjk@defunct.ch>
2015-07-14 14:28:47 +0200

    test: more testing

---
+++ spec/features/mark_read_uses_js_spec.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+require "rails_helper"
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "marking as read uses JS" do
+  let(:message) { create(:cf_message) }
+  let(:user) { create(:cf_user) }
+
+  before(:each) { login_as(user , scope: :user) }
+
+  include CForum::Tools
+
+  it "doesn't reload when clicking the mark read icon", js: true do
+    visit cf_forum_path(message.forum)
+
+    page.find('#t' + message.thread_id.to_s + ' .icon-thread.mark-thread-read').click
+    sleep 0.1
+    expect(page.body).to have_css('#m' + message.message_id.to_s + '.visited')
+  end
+
+end
+
+# eof
---
+++ spec/features/open_close_threads_uses_js_spec.rb
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+require "rails_helper"
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "opening/closing threads uses JS" do
+  let(:message) { create(:cf_message) }
+  let(:user) { create(:cf_user) }
+
+  before(:each) { login_as(user , scope: :user) }
+
+  include CForum::Tools
+
+  it "doesn't reload when clicking the open/close icon", js: true do
+    visit cf_forum_path(message.forum)
+
+    page.find('#t' + message.thread_id.to_s + ' .icon-thread.open').click
+    sleep 0.1
+    expect(page.body).to have_css('#t' + message.thread_id.to_s + ' .icon-thread.closed')
+
+    page.find('#t' + message.thread_id.to_s + ' .icon-thread.closed').click
+    sleep 0.1
+    expect(page.body).to have_css('#t' + message.thread_id.to_s + ' .icon-thread.open')
+  end
+
+end
+
+# eof
