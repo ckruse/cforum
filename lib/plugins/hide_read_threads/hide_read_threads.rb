e346214b885fb2b8541f520f44fa95792a184edb
Christian Kruse <cjk@defunct.ch>
2015-09-16 22:36:54 +0200

    ref: don’t hide threads with interesting messages
    in „hide read threads“ don’t hide threads with interesting messages

--- lib/plugins/hide_read_threads/hide_read_threads.rb
+++ lib/plugins/hide_read_threads/hide_read_threads.rb
@@ -11,7 +11,8 @@ class HideReadThreadsPlugin < Plugin
     return if current_user.blank? or get('view_all') or uconf('hide_read_threads') != 'yes' or session[:srt]
 
     return Proc.new { |threads|
-      threads.where("EXISTS(SELECT a.message_id FROM messages a LEFT JOIN read_messages b ON a.message_id = b.message_id AND b.user_id = ? WHERE thread_id = threads.thread_id AND read_message_id IS NULL AND a.deleted = false)", current_user.user_id)
+      threads.where("EXISTS(SELECT a.message_id FROM messages a LEFT JOIN read_messages b ON a.message_id = b.message_id AND b.user_id = ? WHERE thread_id = threads.thread_id AND read_message_id IS NULL AND a.deleted = false) OR EXISTS(SELECT a.message_id FROM messages AS a INNER JOIN interesting_messages USING(message_id) WHERE thread_id = threads.thread_id AND interesting_messages.user_id = ? AND deleted = false)",
+                    current_user.user_id, current_user.user_id)
     }
   end
 end
