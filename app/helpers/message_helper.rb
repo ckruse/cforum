d044b25767bf6da014a81e3d77746f6ad795d683
Matthias Apsel <matthias.apsel@selfhtml.org>
2015-11-21 20:37:31 +0100

    Link auf Versionen weniger ausführlich

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -239,32 +239,11 @@ module MessageHelper
     end
 
     if opts[:show_editor] && !message.edit_author.blank?
-      html << ", <span class=\"versions\">" << cf_link_to(t('messages.edited_by'),
+      html << " <span class=\"versions\">(" << cf_link_to(t('messages.versions'),
                                                           versions_cf_message_path(thread, message),
                                                           rel: 'no-follow',
                                                           class: 'version-link')
-
-      if message.editor_id.blank?
-        html << " <span class=\"editor\">".html_safe << cf_link_to(message.edit_author,
-                                                                  versions_cf_message_path(thread, message),
-                                                                  rel: 'no-follow',
-                                                                  class: 'version-link') << "</span> ".html_safe
-      else
-        html << " <span class=\"registered-user editor\">".html_safe <<
-          cf_link_to(image_tag(message.editor.avatar(:thumb), class: 'avatar'),
-                     user_path(message.editor),
-                     title: t('messages.user_link', user: message.editor.username),
-                     class: 'user-link') << " " << cf_link_to(message.edit_author,
-                                                              versions_cf_message_path(thread, message),
-                                                              rel: 'no-follow',
-                                                              class: 'version-link') << "</span> ".html_safe
-      end
-
-      html << "<time class=\"edit-time\" datetime=\"" << message.updated_at.strftime("%FT%T%:z") << '">' <<
-        encode_entities(l(message.updated_at, format: date_format("date_format_post"))) <<
-        "</time>"
-
-      html << "</span>"
+      html << ")</span>"
     end
 
     if not message.tags.blank? and opts[:tags]
