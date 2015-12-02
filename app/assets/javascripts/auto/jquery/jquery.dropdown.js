1e629632a67dacd4ec8a5cc634c15c9241d56e66
Christian Kruse <cjk@defunct.ch>
2015-12-02 21:30:56 +0100

    fix: dropdown with blur/focus doesn’t work

--- app/assets/javascripts/auto/jquery/jquery.dropdown.js
+++ app/assets/javascripts/auto/jquery/jquery.dropdown.js
@@ -5,7 +5,6 @@
     var $this = $(this);
 
     var anchor = $this.find('.anchor');
-    var menu = $this.find(".menu");
 
     anchor.addClass('visible');
     $this.addClass('js');
@@ -14,13 +13,7 @@
     anchor.on('click', function(ev) {
       ev.preventDefault();
       parent.toggleClass('open');
-
-      if(!parent.hasClass("open")) {
-        parent.blur();
-      }
     });
-
-    parent.on('blur', function() { parent.removeClass('open'); });
   };
 })(jQuery);
 
