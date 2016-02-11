c8dc051752800baeeafb4eed4bddeb9fba8bfbac
Christian Kruse <cjk@defunct.ch>
2016-02-11 20:08:38 +0100

    ref: some tests for messages controller

--- spec/controllers/cf_messages_controller_spec.rb
+++ spec/controllers/cf_messages_controller_spec.rb
@@ -43,6 +43,26 @@ RSpec.describe CfMessagesController, type: :controller do
 
       expect(response.body).to have_css('.versions')
     end
+
+    it "redirects to correct form when accessing via wrong forum" do
+      forum1 = create(:cf_write_forum)
+      message.forum = forum1
+
+      get :show, message_params_from_slug(message).merge(curr_forum: forum1.slug)
+
+      expect(response).to redirect_to cf_message_url(message.thread, message)
+    end
+
+    it "sets readmode cookie when overwriting via query string" do
+      get :show, message_params_from_slug(message).merge(rm: 'thread-view')
+      expect(response.cookies['cf_readmode']).to eq 'thread-view'
+    end
+
+    it "doesn't set readmode cookie when overwriting via query string and signed in" do
+      sign_in admin
+      get :show, message_params_from_slug(message).merge(rm: 'thread-view')
+      expect(response.cookies['cf_readmode']).to be_nil
+    end
   end
 
   describe "GET #new" do
@@ -96,6 +116,32 @@ RSpec.describe CfMessagesController, type: :controller do
     end
   end
 
+  describe "GET #edit" do
+    it "shows the edit form as admin" do
+      sign_in admin
+      get :edit, message_params_from_slug(message)
+      expect(response).to render_template "edit"
+    end
+
+    it "shows the edit form as owner" do
+      sign_in message.owner
+      get :edit, message_params_from_slug(message)
+      expect(response).to render_template "edit"
+    end
+
+    it "redirects when trying to edit as anonymous" do
+      get :edit, message_params_from_slug(message)
+      expect(response).to redirect_to cf_message_url(message.thread, message)
+    end
+
+    it "redirects when trying to edit as wrong user" do
+      user1 = create(:cf_user)
+      sign_in user1
+      get :edit, message_params_from_slug(message)
+      expect(response).to redirect_to cf_message_url(message.thread, message)
+    end
+  end
+
   describe "POST #update" do
     it "updates a message to markdown" do
       message.format = 'cforum'
@@ -121,6 +167,32 @@ RSpec.describe CfMessagesController, type: :controller do
         post :update, message_params_from_slug(message).merge(cf_message: message.attributes,
                                                               tags: ['rebellion'])
       }.to change(message.versions, :count).by(0)
+
+    end
+
+    it "renders new when anon" do
+      post :update, message_params_from_slug(message).merge(cf_message: message.attributes,
+                                                            tags: ['rebellion'])
+      expect(response).to render_template "new"
+    end
+
+    it "renders new when wrong user" do
+      user1 = create(:cf_user)
+      sign_in user1
+      post :update, message_params_from_slug(message).merge(cf_message: message.attributes,
+                                                            tags: ['rebellion'])
+      expect(response).to  render_template "new"
+    end
+
+    it "updates search index when content changed" do
+      sign_in admin
+
+      message.author = "Jar Jar Binks"
+
+      expect {
+        post :update, message_params_from_slug(message).merge(cf_message: message.attributes,
+                                                              tags: ['rebellion'])
+      }.to change(SearchDocument, :count).by(1)
 
     end
   end
