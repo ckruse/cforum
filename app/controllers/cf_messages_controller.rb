fdcfc568008a19b525568eed185c7c1929f884e3
Christian Kruse <cjk@defunct.ch>
2016-02-17 10:17:54 +0100

    ref: ignore empty lines and comments

--- app/controllers/cf_messages_controller.rb
+++ app/controllers/cf_messages_controller.rb
@@ -424,10 +424,12 @@ class CfMessagesController < ApplicationController
     content_black_list = conf('content_black_list').to_s.split(/\015\012|\015|\012/)
 
     subject_black_list.each do |expr|
+      next if expr =~ /^#/ or expr =~ /^\s*$/
       return true if Regexp.new(expr, Regexp::IGNORECASE).match(msg.subject)
     end
 
     content_black_list.each do |expr|
+      next if expr =~ /^#/ or expr =~ /^\s*$/
       return true if Regexp.new(expr, Regexp::IGNORECASE).match(msg.content)
     end
 
