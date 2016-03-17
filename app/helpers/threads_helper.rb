6080cfd39459ceade01a8754502228a82863c8ef
Christian Kruse <cjk@defunct.ch>
2016-03-17 21:05:37 +0100

    fix: don’t hide read threads when in archive mode
    Fixes #593

--- app/helpers/threads_helper.rb
+++ app/helpers/threads_helper.rb
@@ -6,7 +6,7 @@ module ThreadsHelper
       session.delete :srt
     end
 
-    return threads if current_user.blank? or @view_all or uconf('hide_read_threads') != 'yes' or session[:srt]
+    return threads if current_user.blank? or @view_all or uconf('hide_read_threads') != 'yes' or session[:srt] or controller_path == 'cf_archive'
 
     threads.where("EXISTS(SELECT a.message_id FROM messages a LEFT JOIN read_messages b ON a.message_id = b.message_id AND b.user_id = ? WHERE thread_id = threads.thread_id AND read_message_id IS NULL AND a.deleted = false) OR EXISTS(SELECT a.message_id FROM messages AS a INNER JOIN interesting_messages USING(message_id) WHERE thread_id = threads.thread_id AND interesting_messages.user_id = ? AND deleted = false)",
                   current_user.user_id, current_user.user_id)
