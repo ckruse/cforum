45516632a348d6b0ff9dacda81a3e657cab1b9a9
Christian Kruse <cjk@defunct.ch>
2016-03-24 10:13:25 +0100

    fix: use protocol from config, too

--- lib/peon.rb
+++ lib/peon.rb
@@ -18,7 +18,7 @@ module Peon
       end
 
       def root_url
-        'http://' + ActionMailer::Base.default_url_options[:host] + root_path
+        (ActionMailer::Base.default_url_options[:protocol] || 'http') + '://' + ActionMailer::Base.default_url_options[:host] + root_path
       end
 
       def give_badge(user, badge)
