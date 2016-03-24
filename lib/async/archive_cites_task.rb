358ba64570cbd67ea0deb54936f9d53ae560ebe6
Christian Kruse <cjk@defunct.ch>
2016-03-24 11:52:45 +0100

    ref: removed cite backups

--- lib/async/archive_cites_task.rb
+++ lib/async/archive_cites_task.rb
@@ -26,28 +26,6 @@ module Peon
         doc.save!
       end
 
-      def backup_file(cite, file)
-        xml = <<-XML
-<?xml encoding="utf-8"?>
-<cite>
-  <old_id>#{cite.old_id}</old_id>
-  <user_id>#{cite.user_id}</user_id>
-  <message_id>#{cite.message_id}</message_id>
-  <url><![CDATA[#{cite.url}]]></url>
-  <author><![CDATA[#{cite.author}]]></author>
-  <creator><![CDATA[#{cite.creator}]]></creator>
-  <cite><![CDATA[#{cite.cite}]]></cite>
-  <created_at>#{cite.created_at}</created_at>
-  <updated_at>#{cite.updated_at}</updated_at>
-  <cite_date>#{cite.cite_date}</cite_date>
-</cite>
-        XML
-
-        File.open(file, "w:utf-8") do |fd|
-          fd.write(xml)
-        end
-      end
-
       def work_work(args)
         min_age = conf('cites_min_age_to_archive', nil).to_i
         cites = CfCite.
@@ -66,11 +44,6 @@ module Peon
             audit(cite, 'archive', nil)
 
           else
-            backup = (Rails.root + 'tmp/').to_s + cite.cite_id.to_s + '.xml'
-            Rails.logger.info "Trashing cite #{cite.cite_id}, backup at #{backup}"
-
-            backup_file cite, backup
-
             cite.destroy
             audit(cite, 'destroy', nil)
           end
