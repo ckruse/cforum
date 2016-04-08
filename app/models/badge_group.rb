b5e92e0e334efde69f99561339dd6ae805cd0d28
Christian Kruse <cjk@defunct.ch>
2016-04-08 16:04:08 +0200

    ref: badges are now ordered by pos by default

--- app/models/badge_group.rb
+++ app/models/badge_group.rb
@@ -5,7 +5,7 @@ class BadgeGroup < ActiveRecord::Base
   self.table_name  = 'badge_groups'
 
   has_many :badge_badge_groups, dependent: :delete_all
-  has_many :badges, through: :badge_badge_groups
+  has_many :badges, ->{ order(:order) }, through: :badge_badge_groups
 
   validates :name, presence: true, length: {in: 2..255}, allow_blank: false, uniqueness: { case_sensitive: false }
 
