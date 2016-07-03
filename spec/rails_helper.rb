5c5716c50fb2a5eff6b3d72207ffb909f8b60cf2
Christian Kruse <cjk@defunct.ch>
2016-07-03 12:23:34 +0200

    bundle update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,8 +59,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::TestHelpers, type: :controller
-  config.include Devise::TestHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   config.include WaitForAjax, type: :feature
 
   config.include Warden::Test::Helpers
