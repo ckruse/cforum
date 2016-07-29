facc2d82c1fa3b0c74e8346b48f8efe124291319
Christian Kruse <cjk@defunct.ch>
2016-07-29 12:55:04 +0200

    fix: fixed test

--- spec/views/attendees/new.html.erb_spec.rb
+++ spec/views/attendees/new.html.erb_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "attendees/new", type: :view do
 
     assert_select "form[action=?][method=?]", event_attendees_path(event), "post" do
       assert_select "input#attendee_name[name=?]", "attendee[name]"
-      assert_select "input#attendee_comment[name=?]", "attendee[comment]"
+      assert_select "textarea#attendee_comment[name=?]", "attendee[comment]"
       assert_select "input#attendee_starts_at[name=?]", "attendee[starts_at]"
       assert_select "input#attendee_seats[name=?]", "attendee[seats]"
     end
