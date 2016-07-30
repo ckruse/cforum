bd5b790bd182c286c9e93605c2321a3e3c2f92f0
Christian Kruse <cjk@defunct.ch>
2016-07-30 08:49:53 +0200

    fix: validate uniqueness of user_id only if not blank

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -4,7 +4,7 @@ class Attendee < ActiveRecord::Base
   self.primary_key = 'attendee_id'
 
   validates_presence_of :name, :planned_arrival
-  validates_uniqueness_of :user_id, scope: :event_id
+  validates_uniqueness_of :user_id, scope: :event_id, allow_blank: true
 
   belongs_to :event
   belongs_to :user
