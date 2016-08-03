317223e98decde9fe509cda67d746a14d13ff3e5
Christian Kruse <cjk@defunct.ch>
2016-08-03 22:55:37 +0200

    testing admin/users

---
+++ spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+require 'rails_helper'
+
+RSpec.describe Admin::UsersController, type: :controller do
+  let(:admin) { FactoryGirl.create(:user_admin) }
+
+  before(:each) do
+    sign_in admin
+  end
+
+  describe "GET #index" do
+    it "assigns all users as @users" do
+      get :index, {}
+      expect(assigns(:users)).to eq([admin])
+    end
+
+    it "searches user with s param" do
+      users = [create(:user), create(:user), create(:user)]
+      get :index, s: users.first.username
+      expect(assigns(:users)).to eq([users.first])
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new user as @user" do
+      get :new, {}
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested user as @user" do
+      user = create(:user)
+      get :edit, id: user.to_param
+      expect(assigns(:user)).to eq(user)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new user" do
+        expect {
+          post :create, {user: attributes_for(:user)}
+        }.to change(User, :count).by(1)
+      end
+
+      it "assigns a newly created user as @user" do
+        post :create, user: attributes_for(:user)
+        expect(assigns(:user)).to be_a(User)
+        expect(assigns(:user)).to be_persisted
+      end
+
+      it "redirects to the user" do
+        post :create, user: attributes_for(:user)
+        expect(response).to redirect_to(edit_admin_user_url(assigns(:user)))
+      end
+    end
+
+    context "with invalid params" do
+      let(:invalid_attributes) {
+        {username: ""}
+      }
+
+      it "assigns a newly created but unsaved user as @user" do
+        post :create, user: invalid_attributes
+        expect(assigns(:user)).to be_a_new(User)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, user: invalid_attributes
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        {username: 'Foo 1'}
+      }
+
+      it "updates the requested user" do
+        user = create(:user)
+        put :update, id: user.to_param, user: new_attributes
+        user.reload
+        expect(user.username).to eq 'Foo 1'
+      end
+
+      it "assigns the requested user as @user" do
+        user = create(:user)
+        put :update, id: user.to_param, user: new_attributes
+        expect(assigns(:user)).to eq(user)
+      end
+
+      it "redirects to the user" do
+        user = create(:user)
+        put :update, id: user.to_param, user: new_attributes
+        expect(response).to redirect_to(edit_admin_user_url(user))
+      end
+    end
+
+    context "with invalid params" do
+      let(:invalid_attributes) {
+        {username: ''}
+      }
+
+      it "assigns the user as @user" do
+        user = create(:user)
+        put :update, id: user.to_param, user: invalid_attributes
+        expect(assigns(:user)).to eq(user)
+      end
+
+      it "re-renders the 'edit' template" do
+        user = create(:user)
+        put :update, id: user.to_param, user: invalid_attributes
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested user" do
+      user = create(:user)
+      expect {
+        delete :destroy, id: user.to_param
+      }.to change(User, :count).by(-1)
+    end
+
+    it "redirects to the users list" do
+      user = create(:user)
+      delete :destroy, id: user.to_param
+      expect(response).to redirect_to(admin_users_url)
+    end
+  end
+
+end
+
+# eof
