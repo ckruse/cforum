f1104ca7c31a74def751d4072cdf3d9ee417db01
Christian Kruse <cjk@defunct.ch>
2016-08-06 11:19:28 +0200

    ref: notification mailer spec

---
+++ spec/mailers/notification_mailer_spec.rb
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+require "rails_helper"
+
+RSpec.describe NotificationMailer, type: :mailer do
+  include CForum::Tools
+
+  describe 'new notification' do
+    let(:user) { create(:user) }
+    let(:notification) { create(:notification) }
+    let(:mail) { NotificationMailer.new_notification({user: user, subject: notification.subject,
+                                                      body: "Foo bar"}) }
+
+    it 'renders the subject as defined' do
+      expect(mail.subject).to eq(notification.subject)
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq([Rails.application.config.mail_sender])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to match("Foo bar")
+    end
+  end
+end
+
+# eof
