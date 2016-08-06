4fccaa780ddb690a035b5c69ca79bffde31230e2
Christian Kruse <cjk@defunct.ch>
2016-08-06 10:45:51 +0200

    ref: notify new testing

--- app/mailers/notify_new_mailer.rb
+++ app/mailers/notify_new_mailer.rb
@@ -52,6 +52,10 @@ class NotifyNewMailer < ActionMailer::Base
     @url     = url
     @txt     = txt_content
 
+    headers['Message-Id'] = '<t' + @message.thread_id.to_s + "m" +
+                            @message.message_id.to_s + "@" +
+                            Rails.application.config.mid_host + ">"
+
     mail(
       from: Rails.application.config.mail_sender,
       to: user.email,
@@ -63,6 +67,8 @@ class NotifyNewMailer < ActionMailer::Base
     @user  = user
     @cite  = cite
     @url   = url
+
+    headers['Message-Id'] = '<cite' + @cite.cite_id.to_s + "@" + Rails.application.config.mid_host + ">"
 
     mail(
       from: Rails.application.config.mail_sender,
---
+++ spec/mailers/notify_new_mailer_spec.rb
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+require "rails_helper"
+
+RSpec.describe NotifyNewMailer, type: :mailer do
+  include CForum::Tools
+
+  describe 'new message' do
+    let(:user) { create(:user) }
+    let(:parent) { create(:message) }
+    let(:message) { create(:message, parent: parent, thread: parent.thread) }
+    let(:mail) { NotifyNewMailer.new_message(user, message.thread, parent,
+                                             message, message_path(message.thread, message),
+                                             'foo bar').deliver_now }
+
+    it 'renders the subject as RE: message subject' do
+      expect(mail.subject).to eq('RE: ' + message.subject)
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq([Rails.application.config.mail_sender])
+    end
+
+    it "sets the Message-Id" do
+      expect(mail).to have_header('Message-Id',
+                                  '<t' + message.thread_id.to_s + "m" +
+                                  message.message_id.to_s + "@" + Rails.application.config.mid_host + ">")
+    end
+    it "sets the In-Reply-To" do
+      expect(mail).to have_header('In-Reply-To',
+                                  '<t' + parent.thread_id.to_s + 'm' +
+                                  parent.message_id.to_s + "@" + Rails.application.config.mid_host + ">")
+    end
+
+    it 'links to the message' do
+      expect(mail.body.encoded).to match(message_path(message.thread, message))
+    end
+  end
+
+  describe 'new answer' do
+    let(:user) { create(:user) }
+    let(:parent) { create(:message) }
+    let(:message) { create(:message, parent: parent, thread: parent.thread) }
+    let(:mail) { NotifyNewMailer.new_answer(user, message.thread, parent,
+                                            message, message_path(message.thread, message),
+                                            'foo bar').deliver_now }
+
+    it 'renders the subject as RE: message subject' do
+      expect(mail.subject).to eq('RE: ' + message.subject)
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq([Rails.application.config.mail_sender])
+    end
+
+    it "sets the Message-Id" do
+      expect(mail).to have_header('Message-Id',
+                                  '<t' + message.thread_id.to_s + "m" +
+                                  message.message_id.to_s + "@" + Rails.application.config.mid_host + ">")
+    end
+    it "sets the In-Reply-To" do
+      expect(mail).to have_header('In-Reply-To',
+                                  '<t' + parent.thread_id.to_s + 'm' +
+                                  parent.message_id.to_s + "@" + Rails.application.config.mid_host + ">")
+    end
+
+    it 'links to the message' do
+      expect(mail.body.encoded).to match(message_path(message.thread, message))
+    end
+  end
+
+  describe 'new mention' do
+    let(:user) { create(:user) }
+    let(:message) { create(:message) }
+    let(:mail) { NotifyNewMailer.new_mention(user, message.thread, message,
+                                             message_path(message.thread, message),
+                                             'foo bar').deliver_now }
+
+    it 'renders the subject as RE: message subject' do
+      expect(mail.subject).to eq('RE: ' + message.subject)
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq([Rails.application.config.mail_sender])
+    end
+
+    it "sets the Message-Id" do
+      expect(mail).to have_header('Message-Id',
+                                  '<t' + message.thread_id.to_s + "m" +
+                                  message.message_id.to_s + "@" + Rails.application.config.mid_host + ">")
+    end
+
+    it 'links to the message' do
+      expect(mail.body.encoded).to match(message_path(message.thread, message))
+    end
+  end
+
+  describe 'new cite' do
+    let(:user) { create(:user) }
+    let(:cite) { create(:cite) }
+    let(:mail) { NotifyNewMailer.new_cite(user, cite, cite_url(cite)).deliver_now }
+
+    it 'renders the subject as "cites.new_cite_arrived"' do
+      expect(mail.subject).to eq(I18n.t('cites.new_cite_arrived'))
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq([Rails.application.config.mail_sender])
+    end
+
+    it "sets the Message-Id" do
+      expect(mail).to have_header('Message-Id',
+                                  '<cite' + cite.cite_id.to_s + "@" + Rails.application.config.mid_host + ">")
+    end
+
+    it 'links to the cite' do
+      expect(mail.body.encoded).to match(cite_path(cite))
+    end
+  end
+end
+
+# eof
