e9a3893c263db2121ffac0f9b3f97c0941988109
Christian Kruse <cjk@defunct.ch>
2016-11-21 22:04:55 +0100

    ref: respect style guide

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,19 +2,26 @@
 
 class PagesController < ApplicationController
   def help
-    @moderators = User.
-                  where("admin = true OR user_id IN (SELECT user_id FROM groups_users WHERE group_id IN (SELECT group_id FROM forums_groups_permissions WHERE permission = ?)) OR user_id IN (SELECT user_id FROM badges_users INNER JOIN badges USING(badge_id) WHERE badge_type = ?)",
-                        ForumGroupPermission::ACCESS_MODERATE, Badge::MODERATOR_TOOLS).
-                  order(:username)
+    @moderators = User
+                    .where('admin = true OR' \
+                           '  user_id IN (SELECT user_id FROM groups_users WHERE group_id IN ' \
+                           '    (SELECT group_id FROM forums_groups_permissions WHERE permission = ?)) OR' \
+                           '  user_id IN (SELECT user_id FROM badges_users INNER JOIN badges USING(badge_id)' \
+                           '              WHERE badge_type = ?)',
+                           ForumGroupPermission::ACCESS_MODERATE, Badge::MODERATOR_TOOLS)
+                    .order(:username)
 
-    @badge_groups = BadgeGroup.preload(:badges).order(:name).all
+    @badge_groups = BadgeGroup
+                      .preload(:badges)
+                      .order(:name)
+                      .all
 
-    @cites = Cite.
-             select("date_trunc('month', created_at) AS created_at, COUNT(*) AS cnt").
-             where("created_at >= NOW() - INTERVAL '12 months'").
-             group("date_trunc('month', created_at)").
-             order("date_trunc('month', created_at)").
-             all
+    @cites = Cite
+               .select("date_trunc('month', created_at) AS created_at, COUNT(*) AS cnt")
+               .where("created_at >= NOW() - INTERVAL '12 months'")
+               .group("date_trunc('month', created_at)")
+               .order("date_trunc('month', created_at)")
+               .all
   end
 end
 
