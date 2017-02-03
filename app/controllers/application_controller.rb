c913f5cb41b8b52b14ee996fccc1fa8778756976
Christian Kruse <cjk@defunct.ch>
2017-02-03 12:12:29 +0100

    ref: prepend forgery protection to get back the old behaviour

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protect_from_forgery
+  protect_from_forgery prepend: true
 
   #
   # normal stuff
