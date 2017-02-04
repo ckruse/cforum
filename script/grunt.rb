5355d75879c013c8feb9572ad9f35898e71b2f28
Christian Kruse <cjk@defunct.ch>
2017-02-04 10:25:11 +0100

    ref: respect style guide

--- script/grunt.rb
+++ script/grunt.rb
@@ -2,15 +2,14 @@
 # -*- encoding: utf-8 -*-
 
 require File.join(File.dirname(__FILE__), '..', 'lib', 'peon.rb')
-require File.join(File.dirname(__FILE__), "..", "lib", "peon", "grunt.rb")
+require File.join(File.dirname(__FILE__), '..', 'lib', 'peon', 'grunt.rb')
 
 ActiveRecord::Base.record_timestamps = false
-Rails.logger = Logger.new(File.join(File.dirname(__FILE__), "..", 'log', 'grunt.log'))
+Rails.logger = Logger.new(File.join(File.dirname(__FILE__), '..', 'log', 'grunt.log'))
 ActiveRecord::Base.logger = Rails.logger
 
 grunt = Peon::Grunt.instance
 grunt.init(['peon'])
 grunt.run
-
 
 # eof
