819e6b48e527006e2b9cd04b6abcb627068756d4
Christian Kruse <cjk@defunct.ch>
2017-02-11 17:24:59 +0100

    ref: fix replacement

--- app/assets/javascripts/auto/mentions.js
+++ app/assets/javascripts/auto/mentions.js
@@ -39,8 +39,8 @@ cforum.mentions = function(elem) {
 
     {
       id: 'typography',
-      match: /\s+(=>|<=|<=>|,,|...|\*|->|<-|-{1,3}|\^|\[tm\]?|=\/=?|=)$/,
-      index: 1,
+      match: /(\s+)(=>|<=|<=>|,,|...|\*|->|<-|-{1,3}|\^|\[tm\]?|=\/=?|=)$/,
+      index: 2,
       search: function (term, callback) {
         var found = [];
 
@@ -94,7 +94,7 @@ cforum.mentions = function(elem) {
 
         callback(found);
       },
-      replace: function(text) { return text; }
+      replace: function(text) { return '$1' + text; }
     }
   ], { maxCount: 750 })
     .on("textComplete:render", function(ev, menu) {
