c7c4a665fb9fcd1aa904222097422e2a151a0840
Christian Kruse <cjk@defunct.ch>
2017-02-26 10:17:50 +0100

    ref: refactoring, avoid shallow-ing

--- app/helpers/link_tags_helper.rb
+++ app/helpers/link_tags_helper.rb
@@ -1,43 +1,46 @@
 # -*- coding: utf-8 -*-
 
 module LinkTagsHelper
-  def to_shallow(msgs, ary)
-    msgs.each do |m|
-      ary << m
-      to_shallow(m.messages, ary) unless m.messages.blank?
-    end
-  end
-
   def top_link
     '<link rel="index" href="' + forum_path(current_forum) + '" title="' +
       encode_entities(current_forum ? current_forum.name : I18n.t('forums.all_forums')) + '">'
   end
 
-  def first_link(thread, msgs)
-    '<link rel="first" href="' + message_path(thread, msgs[0]) + '" title="' + t('plugins.link_tags.first_msg') + '">'
+  def first_link(thread)
+    '<link rel="first" href="' + message_path(thread, thread.sorted_messages[0]) +
+      '" title="' + t('plugins.link_tags.first_msg') + '">'
   end
 
-  def last_link(thread, msgs)
-    '<link rel="last" href="' + message_path(thread, msgs[-1]) + '" title="' + t('plugins.link_tags.last_msg') + '">'
+  def last_link(thread)
+    '<link rel="last" href="' + message_path(thread, thread.sorted_messages[-1]) +
+      '" title="' + t('plugins.link_tags.last_msg') + '">'
   end
 
-  def prev_link(thread, msgs, message)
-    msgs.each_with_index do |m, i|
-      if m.message_id == message.message_id
-        return '<link rel="prev" href="' + message_path(thread, msgs[i - 1]) + '" title="' + t('plugins.link_tags.prev_msg') + '">' if i > 0
-        return ''
+  def prev_link(thread, message)
+    thread.sorted_messages.each_with_index do |m, i|
+      next if m.message_id != message.message_id
+
+      if i > 0
+        return '<link rel="prev" href="' + message_path(thread, thread.sorted_messages[i - 1]) +
+               '" title="' + t('plugins.link_tags.prev_msg') + '">'
       end
+
+      return ''
     end
 
     ''
   end
 
-  def next_link(thread, msgs, message)
-    msgs.each_with_index do |m, i|
-      if m.message_id == message.message_id
-        return '<link rel="next" href="' + message_path(thread, msgs[i + 1]) + '" title="' + t('plugins.link_tags.next_msg') + '">' if msgs[i + 1] # TODO: Localize
-        return ''
+  def next_link(thread, message)
+    thread.sorted_messages.each_with_index do |m, i|
+      next if m.message_id != message.message_id
+
+      if thread.sorted_messages[i + 1]
+        return '<link rel="next" href="' + message_path(thread, thread.sorted_messages[i + 1]) +
+               '" title="' + t('plugins.link_tags.next_msg') + '">'
       end
+
+      return ''
     end
 
     ''
@@ -48,14 +51,11 @@ module LinkTagsHelper
   end
 
   def show_thread_link_tags(thread, message = nil)
-    msgs = []
-    to_shallow([thread.message], msgs)
-
     html = top_link
-    html << "\n" + first_link(thread, msgs)
-    html << "\n" + last_link(thread, msgs)
-    html << "\n" + next_link(thread, msgs, message)
-    html << "\n" + prev_link(thread, msgs, message)
+    html << "\n" + first_link(thread)
+    html << "\n" + last_link(thread)
+    html << "\n" + next_link(thread, message)
+    html << "\n" + prev_link(thread, message)
 
     @link_tags = html.html_safe
   end
@@ -65,10 +65,10 @@ module LinkTagsHelper
     to_shallow([thread.message], msgs)
 
     html = top_link
-    html << "\n" + first_link(thread, msgs)
-    html << "\n" + last_link(thread, msgs)
-    html << "\n" + next_link(thread, msgs, message)
-    html << "\n" + prev_link(thread, msgs, message)
+    html << "\n" + first_link(thread)
+    html << "\n" + last_link(thread)
+    html << "\n" + next_link(thread, message)
+    html << "\n" + prev_link(thread, message)
 
     @link_tags = html.html_safe
   end
