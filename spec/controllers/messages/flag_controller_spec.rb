5daf6b534fa492f2b4d21773308380337534d137
Christian Kruse <cjk@defunct.ch>
2017-05-25 09:39:50 +0200

    ref: fixed tests

--- spec/controllers/messages/flag_controller_spec.rb
+++ spec/controllers/messages/flag_controller_spec.rb
@@ -12,116 +12,85 @@ RSpec.describe Messages::FlagController do
       get :flag, params: message_params_from_slug(message)
       expect(assigns(:message)).to eq(message)
     end
-
-    it 'redirects to message when message is already flagged' do
-      message.flags_will_change!
-      message.flags['flagged'] = 'off-topic'
-      message.save!
-
-      get :flag, params: message_params_from_slug(message)
-
-      expect(response).to redirect_to(message_url(message.thread, message))
-      expect(flash[:notice]).to be_present
-    end
   end
 
   describe 'POST #flagging' do
     context 'success' do
       it 'flags a message' do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'off-topic')
-        message.reload
-        expect(message.flags['flagged']).to eq 'off-topic'
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'off-topic' }))
+        end.to change(ModerationQueueEntry, :count).by(1)
       end
 
       it 'flags a message with a duplicate URL' do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'duplicate',
-                                                                        duplicate_slug: message_url(message.thread, message))
-        message.reload
-        expect(message.flags['flagged']).to eq 'duplicate'
-        expect(message.flags['flagged_dup_url']).to eq message_url(message.thread, message)
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'duplicate',
+                                                          duplicate_url: message_url(message.thread, message) }))
+        end.to change(ModerationQueueEntry, :count).by(1)
       end
+
       it 'flags a message with custom reason' do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'custom',
-                                                                        custom_reason: 'foo bar foo bar')
-        message.reload
-        expect(message.flags['flagged']).to eq 'custom'
-        expect(message.flags['custom_reason']).to eq 'foo bar foo bar'
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'custom',
+                                                          custom_reason: 'foo bar foo bar' }))
+        end.to change(ModerationQueueEntry, :count).by(1)
       end
 
       it 'redirects to message' do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'off-topic')
+        post(:flagging,
+             params: message_params_from_slug(message)
+                       .merge(moderation_queue_entry: { reason: 'off-topic' }))
         expect(response).to redirect_to(message_url(message.thread, message))
+      end
+
+      it 'increases reported counter on multiple reports' do
+        entry = ModerationQueueEntry.create!(message_id: message.message_id,
+                                             reason: 'off-topic',
+                                             reported: 1)
+
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'off-topic' }))
+        end.to change(ModerationQueueEntry, :count).by(0)
+
+        entry.reload
+        expect(entry.reported).to eq 2
       end
     end
 
     context 'fail' do
-      it "doesn't flag an already flagged message" do
-        message.flags_will_change!
-        message.flags['flagged'] = 'off-topic'
-        message.save
-
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'not-constructive')
-
-        expect(response).to redirect_to(message_url(message.thread, message))
-        expect(flash[:notice]).to be_present
-      end
-
       it "doesn't flag with a missing custom reason" do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'custom')
-        message.reload
-        expect(message.flags['flagged']).to be nil
-        expect(message.flags['custom_reason']).to be nil
-        expect(flash[:error]).to be_present
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'custom',
+                                                          custom_reason: '' }))
+        end.to change(ModerationQueueEntry, :count).by(0)
       end
 
       it "doesn't flag with a missing duplicate URL" do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'duplicate')
-        message.reload
-        expect(message.flags['flagged']).to be nil
-        expect(message.flags['flagged_dup_url']).to be nil
-        expect(flash[:error]).to be_present
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'duplicate',
+                                                          duplicate_url: '' }))
+        end.to change(ModerationQueueEntry, :count).by(0)
       end
 
       it "doesn't accept bullshit as reason" do
-        post :flagging, params: message_params_from_slug(message).merge(reason: 'foobar')
-        message.reload
-        expect(message.flags['flagged']).to be nil
-        expect(flash[:error]).to be_present
+        expect do
+          post(:flagging,
+               params: message_params_from_slug(message)
+                         .merge(moderation_queue_entry: { reason: 'some bullshit' }))
+        end.to change(ModerationQueueEntry, :count).by(0)
       end
-    end
-  end
-
-  describe 'GET #unflag' do
-    before(:each) do
-      message.flags_will_change!
-      message.flags['flagged'] = 'custom'
-      message.flags['custom_reason'] = 'custom'
-      message.flags['flagged_dup_url'] = 'custom'
-      message.save!
-    end
-
-    it 'removes flagged' do
-      post :unflag, params: message_params_from_slug(message)
-      message.reload
-      expect(message.flags['flagged']).to be nil
-    end
-
-    it 'removes custom reason' do
-      post :unflag, params: message_params_from_slug(message)
-      message.reload
-      expect(message.flags['custom_reason']).to be nil
-    end
-
-    it 'removen dup url' do
-      post :unflag, params: message_params_from_slug(message)
-      message.reload
-      expect(message.flags['flagged_dup_url']).to be nil
-    end
-
-    it 'redirects to message' do
-      post :unflag, params: message_params_from_slug(message)
-      message.reload
-      expect(response).to redirect_to(message_url(message.thread, message))
     end
   end
 end
