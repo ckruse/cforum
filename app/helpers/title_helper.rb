50577e35e7b71d5d9bff32520a9db56848cd5206
Christian Kruse <cjk@defunct.ch>
2017-06-30 22:11:32 +0200

    ref: refactoring for better readability and fixing some bugs
    e.g. don't count messages in invisible threads

--- app/helpers/title_helper.rb
+++ app/helpers/title_helper.rb
@@ -16,13 +16,26 @@ module TitleHelper
       title << priv_msgs.to_s
     end
 
-    if (uconf('show_new_messages_since_last_visit_in_title') == 'yes') && !current_user.last_sign_in_at.blank?
-      cnt = Message.select('count(*) AS cnt')
-              .joins('LEFT JOIN read_messages ON read_messages.message_id = messages.message_id AND read_messages.user_id = ' + current_user.user_id.to_s + ' INNER JOIN threads USING(thread_id)')
-              .where('messages.forum_id IN (?) AND read_messages.message_id IS NULL AND messages.created_at > ? AND messages.deleted = false AND archived = false',
-                     @forums.map(&:forum_id), current_user.last_sign_in_at).all
+    if uconf('show_new_messages_since_last_visit_in_title') == 'yes'
+      cnt = if current_user.last_sign_in_at.blank?
+              0
+            else
+              Message
+                .joins('LEFT JOIN read_messages ON messages.message_id = read_messages.message_id' \
+                       ' AND read_messages.user_id = ' + current_user.user_id.to_s)
+                .joins('INNER JOIN threads USING(thread_id)')
+                .joins('LEFT JOIN invisible_threads ON invisible_threads.thread_id = threads.thread_id' \
+                       ' AND invisible_threads.user_id = ' + current_user.user_id.to_s)
+                .where('invisible_threads.thread_id IS NULL')
+                .where('messages.forum_id IN (?)', @forums.map(&:forum_id))
+                .where('read_messages.message_id IS NULL')
+                .where('messages.deleted = false')
+                .where('archived = false')
+                .where('messages.created_at >= ?', current_user.last_sign_in_at)
+                .count
+            end
 
-      title << cnt[0].cnt
+      title << cnt
     end
 
     @title_infos = '(' + title.join('/') + ') ' unless title.blank?
