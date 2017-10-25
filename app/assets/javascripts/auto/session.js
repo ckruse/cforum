2bad7ca6ab795bfc534a2b863fcf505b7430091f
Christian Kruse <cjk@defunct.ch>
2017-10-25 13:49:10 +0200

    fix: set `type="button"` on button

--- app/assets/javascripts/auto/session.js
+++ app/assets/javascripts/auto/session.js
@@ -4,14 +4,18 @@
 cforum.devise = {
   sessions: {
     init: function() {
-      $(".login-password").after("<button class=\"show-password\" title=\"" + t("messages.show_password") + "\"></button>");
+      $(".login-password").after(
+        '<button class="show-password" type="button" title="' +
+          t("show_password") +
+          '"></button>'
+      );
       $(".show-password").on("click", function(ev) {
         ev.preventDefault();
 
         var field = $(".login-password");
         var type = field.attr("type");
 
-        type = (type == 'text') ? 'password' : 'text';
+        type = type == "text" ? "password" : "text";
         field.attr("type", type);
       });
       //$(".show-password").css({display: 'inline'});
