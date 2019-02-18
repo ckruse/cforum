12271106cd3dcb2695d6a192973846816ba48ae9
Christian Kruse <cjk@defunct.ch>
2019-02-18 10:15:48 +0100

    ref: `.blank` seems to be true on anonymous users?

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -1,6 +1,6 @@
 class UserChannel < ApplicationCable::Channel
   def subscribed
-    reject if current_user.blank?
+    reject if current_user.blank? || current_user.try(:user_id).blank?
     stream_from "users/#{current_user.user_id}"
   end
 end
